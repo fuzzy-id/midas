31645eb97b0ebb9084501c572ace06b86962ae9d
Thomas Bach <thbach@students.uni-mainz.de>
2012-08-09 16:16:51 +0200

    first test passes

--- crawlcrunch/scripts/crawlcrunch.py
+++ crawlcrunch/scripts/crawlcrunch.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 
+import optparse
 import sys
 
 def main(argv=sys.argv, quiet=False):
     pass
+
+class CrawlCrunchCommand(object):
+    
+    description = "Crawl the companies information from crunchbase.com"
+    usage = "usage: %prog [options] dest_dir"
+    parser = optparse.OptionParser(usage, description=description)
+
+    def __init__(self, argv, quiet=False):
+        self.quiet = quiet
+        self.options, self.args = self.parser.parse_args(argv[1:])
+
+    def run(self):
+        if not self.args:
+            self.out('You must provide a destination directory')
+            return 2
+        return 0
+
+    def out(self, msg): # pragma: no cover
+        if not self.quiet:
+            print msg
