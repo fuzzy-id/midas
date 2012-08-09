0f6131cb0d53d117b8b73444886ca3e098ca98cd
Thomas Bach <thbach@students.uni-mainz.de>
2012-08-09 15:50:19 +0200

    base skeleton

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+from setuptools import setup
+
+py_version = sys.version_info[:2]
+
+tests_require = []
+if py_version == (2, 6):
+    tests_require.append('unittest2')
+
+setup(name='crawlcrunch',
+      version='0.1dev1',
+      description='Crawl crunchbase.com for companies information.',
+      author='Thomas Bach',
+      tests_require=tests_require,
+      test_suite='crawlcrunch.tests',
+      entry_points={
+        'console_scripts': ['crawlcrunch=crawlcrunch.scripts.crawlcrunch:main', ],
+        },
+      )
