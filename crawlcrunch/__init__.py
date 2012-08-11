be2a198398d9550bc15e9b01b437777974c89c96
Thomas Bach <thbach@students.uni-mainz.de>
2012-08-11 17:55:24 +0200

    putting things where they should be

--- crawlcrunch/__init__.py
+++ crawlcrunch/__init__.py
@@ -1,1 +1,25 @@
 # -*- coding: utf-8 -*-
+
+import json
+import os.path
+
+from crawlcrunch.compat import GzipFile
+
+class ZippedJsonFile(object):
+
+    def __init__(self, path):
+        self.path = path
+        self.data = None
+
+    def exists(self):
+        return os.path.isfile(self.path)
+
+    def load(self):
+        with GzipFile(self.path, 'rb') as fp:
+            self.data = json.load(fp)
+
+    def dump(self, data=None):
+        if data is not None:
+            self.data = data
+        with GzipFile(self.path, 'wb') as fp:
+            json.dump(self.data, fp)
--- crawlcrunch/companies.py
+++ crawlcrunch/companies.py
@@ -4,6 +4,7 @@ import json
 import logging
 import os.path
 
+from crawlcrunch import ZippedJsonFile
 from crawlcrunch.compat import GzipFile
 from crawlcrunch.compat import UserList
 from crawlcrunch.compat import url_open
@@ -34,22 +35,3 @@ class CompaniesList(UserList):
         content = url_open(
             'http://api.crunchbase.com/v/1/companies.js')
         self.zipf.dump(json.load(content))
-
-class ZippedJsonFile(object):
-
-    def __init__(self, path):
-        self.path = path
-        self.data = None
-
-    def exists(self):
-        return os.path.isfile(self.path)
-
-    def load(self):
-        with GzipFile(self.path, 'rb') as fp:
-            self.data = json.load(fp)
-
-    def dump(self, data=None):
-        if data is not None:
-            self.data = data
-        with GzipFile(self.path, 'wb') as fp:
-            json.dump(self.data, fp)
