853afdabc88d2a7353e60dbcd83a70e007c6ae90
Thomas Bach <thbach@students.uni-mainz.de>
2012-08-12 19:13:19 +0200

    testing the behavior of mock

--- crawlcrunch/tests/test_std_lib.py
+++ crawlcrunch/tests/test_std_lib.py
@@ -13,3 +13,18 @@ class StdLibTests(unittest.TestCase):
         semaphore.release()
         self.assertTrue(semaphore.acquire(False))
         
+class MockTests(unittest.TestCase):
+
+    def test_setting_return_values(self):
+        import mock
+        m = mock.MagicMock()
+        m.get('companies').not_local.return_value = []
+        companies = m.get('companies')
+        self.assertEqual(companies.not_local(), [])
+
+    def test_sub_mocks(self):
+        import mock
+        m = mock.MagicMock()
+        companies = m.get('companies')
+        companies.update()
+        m.get('companies').update.assert_called_once_with()
