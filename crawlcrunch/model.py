f3f8a611549e94565df4cfca3a186bf8415b60f9
Thomas Bach <thbach@students.uni-mainz.de>
2012-08-13 16:45:53 +0200

    more ctrl chars

--- crawlcrunch/model.py
+++ crawlcrunch/model.py
@@ -67,11 +67,27 @@ class CrunchBaseFetcherMixin(object):
     """ A mixin that sums up the general way to fetch data from
     crunchbase.
     """
+    to_replace = ('\x00',
+                  '\x03', 
+                  '\x0b',
+                  '\x0e',
+                  '\x12', 
+                  '\x14', 
+                  '\x1d',
+                  '\x1e',
+                  '\x1f', 
+                  )
     companies_list_url = 'http://api.crunchbase.com/v/1/companies.js'
     company_url_tpl = 'http://api.crunchbase.com/v/1/company/{0}.js'
 
     def query_url(self): # pragma: no cover
         raise NotImplementedError()
+
+    @classmethod
+    def replace_control_chars(cls, s):
+        for c in cls.to_replace:
+            s = s.replace(c, '')
+        return s
 
     def fetch(self):
         logging.info('Fetching {0}'.format(self.name))
@@ -80,16 +96,7 @@ class CrunchBaseFetcherMixin(object):
         s = content.decode('utf-8')
         # Some companies have control chars in theire description
         # *sigh*
-        to_replace = ('\x00',
-                      '\x03', 
-                      '\x0b',
-                      '\x12', 
-                      '\x14', 
-                      '\x1d',
-                      '\x1f', 
-                      )
-        for r in to_replace:
-            s = s.replace(r, '')
+        s = self.replace_control_chars(s)
         return json.loads(s)
 
 class Node(object):
