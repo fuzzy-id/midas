9d90004608d8253bc891672a2228d9560949c959
Thomas Bach <thbach@students.uni-mainz.de>
2012-08-13 17:17:41 +0200

    base to make more commands

--- crawlcrunch/scripts/__init__.py
+++ crawlcrunch/scripts/__init__.py
@@ -1,1 +1,28 @@
 # -*- coding: utf-8 -*-
+import os.path
+
+class CCBaseCommand(object):
+
+    def __init__(self, argv, quiet=False):
+        self.quiet = quiet
+        self.options, self.args = self.parser.parse_args(argv[1:])
+
+    def check_args(self):
+        if len(self.args) == 0:
+            self.out('You must provide a destination directory')
+            return 2
+        elif len(self.args) > 1:
+            self.out(
+                'You must provide one destination directory, not {0}.'.format(len(self.args)))
+            return 2
+        elif not os.path.isdir(self.args[0]):
+            self.out("The directory '{0}' does not exist!".format(
+                    self.args[0]))
+            self.out('Please, create it first.')
+            return 2
+        return 0
+
+    def out(self, msg): # pragma: no cover
+        if not self.quiet:
+            print(msg)
+
--- crawlcrunch/scripts/cc_update.py
+++ crawlcrunch/scripts/cc_update.py
@@ -6,43 +6,26 @@ import logging
 import optparse
 import sys
 
-from crawlcrunch.model import LocalFilesDir
 from crawlcrunch.crawler import Updater
+from crawlcrunch.model import LocalFilesDir
+from crawlcrunch.scripts import CCBaseCommand
 
 def main(argv=sys.argv, quiet=False):
     command = CCUpdateCommand(argv, quiet)
     return command.run()
 
-class CCUpdateCommand(object):
+class CCUpdateCommand(CCBaseCommand):
     
     description = "Crawl the companies information from crunchbase.com"
     usage = "usage: %prog [options] dest_dir"
     parser = optparse.OptionParser(usage, description=description)
 
-    def __init__(self, argv, quiet=False):
-        self.quiet = quiet
-        self.options, self.args = self.parser.parse_args(argv[1:])
-
     def run(self):
-        if len(self.args) == 0:
-            self.out('You must provide a destination directory')
-            return 2
-        elif len(self.args) > 1:
-            self.out(
-                'You must provide one destination directory, not {0}.'.format(len(self.args)))
-            return 2
-
+        ret = self.check_args()
+        if ret != 0:
+            return ret
         root = LocalFilesDir(self.args[0])
-        if not root.exists():
-            self.out("The directory '{0}' does not exist!".format(
-                    self.args[0]))
-            self.out('Please, create it first.')
-            return 2
         logging.basicConfig(level=logging.DEBUG)
         updater = Updater(root)
         updater.run()
         return 0
-
-    def out(self, msg): # pragma: no cover
-        if not self.quiet:
-            print(msg)
