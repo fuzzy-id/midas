eb1647fe34d6baa22e2d8e7bbd94e8fdcfe8d0ba
Thomas Bach <thbach@students.uni-mainz.de>
2012-08-13 16:46:15 +0200

    we have to make the replacement to find the right column

--- crawlcrunch/tests/__init__.py
+++ crawlcrunch/tests/__init__.py
@@ -60,10 +60,12 @@ def prepare_url_open(url_open, return_dict):
 def help_find_invalid_chars(company_name, line, column): # pragma: no cover
     from crawlcrunch.compat import url_open
     from crawlcrunch.model import LocalFilesDir
+    from crawlcrunch.model import CrunchBaseFetcherMixin
     root = LocalFilesDir('/tmp')
     company = root.get(company_name)
     resp = url_open(company.query_url())
     s = resp.read().decode('utf-8')
+    s = CrunchBaseFetcherMixin.replace_control_chars(s)
     splitted = s.split('\n')
     l = splitted[line-1]
     c = l[column-1]
