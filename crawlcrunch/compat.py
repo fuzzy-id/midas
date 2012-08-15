e352675f31ea4bb69aea40ade2307ce21459d433
Thomas Bach <thbach@students.uni-mainz.de>
2012-08-16 00:10:09 +0200

    making it pep8 compliant

--- crawlcrunch/compat.py
+++ crawlcrunch/compat.py
@@ -5,19 +5,23 @@ import sys
 py_version = sys.version_info[:2]
 PY3 = sys.version_info[0] == 3
 
-if py_version == (2, 6): # pragma: no cover
+if py_version == (2, 6):  # pragma: no cover
     import gzip
+
     class GzipFile(gzip.GzipFile):
         def __enter__(self):
             return self
+
         def __exit__(self, *exc_info):
             return True
-elif PY3: # pragma: no cover
+elif PY3:  # pragma: no cover
     import io
     import gzip
+
     class GzipFile(gzip.GzipFile):
         def __enter__(self):
             return io.TextIOWrapper(self, encoding='UTF-8')
+
         def __exit__(self, *exc_info):
             return True
 else:
@@ -25,30 +29,30 @@ else:
 
 try:
     from StringIO import StringIO
-except ImportError: # pragma: no cover
+except ImportError:  # pragma: no cover
     from io import StringIO
 
-if PY3: # pragma: no cover
+if PY3:  # pragma: no cover
     comp_bytes = bytes
 else:
     comp_bytes = (lambda a, _: bytes(a))
 
-if PY3: # pragma: no cover
+if PY3:  # pragma: no cover
     comp_unicode = str
 else:
     comp_unicode = unicode
 
-if PY3: # pragma: no cover
+if PY3:  # pragma: no cover
     from urllib.request import urlopen as url_open
 else:
     from urllib2 import urlopen as url_open
 
-if PY3: # pragma: no cover
+if PY3:  # pragma: no cover
     from urllib.error import HTTPError
 else:
     from urllib2 import HTTPError
 
-if PY3: # pragma: no cover
+if PY3:  # pragma: no cover
     from collections import UserList
 else:
     from UserList import UserList
