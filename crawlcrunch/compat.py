da37f9d8d3716fabc2239b83d9be1aaf6d36beb8
Thomas Bach <thbach@students.uni-mainz.de>
2012-08-16 00:11:29 +0200

    gardening

--- crawlcrunch/compat.py
+++ crawlcrunch/compat.py
@@ -6,25 +6,32 @@ py_version = sys.version_info[:2]
 PY3 = sys.version_info[0] == 3
 
 if py_version == (2, 6):  # pragma: no cover
+
     import gzip
 
     class GzipFile(gzip.GzipFile):
+
         def __enter__(self):
             return self
 
         def __exit__(self, *exc_info):
             return True
+
 elif PY3:  # pragma: no cover
+
     import io
     import gzip
 
     class GzipFile(gzip.GzipFile):
+
         def __enter__(self):
             return io.TextIOWrapper(self, encoding='UTF-8')
 
         def __exit__(self, *exc_info):
             return True
+
 else:
+
     from gzip import GzipFile
 
 try:
