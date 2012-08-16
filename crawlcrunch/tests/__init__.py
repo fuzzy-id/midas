24f66054b697ce059f192f3e99236325266e9ee4
Thomas Bach <thbach@students.uni-mainz.de>
2012-08-16 21:03:41 +0200

    cleaning up

--- crawlcrunch/tests/__init__.py
+++ crawlcrunch/tests/__init__.py
@@ -60,20 +60,3 @@ def prepare_url_open(url_open, return_dict):
     def side_effect(url):
         return _make_json_buffer(return_dict[url])
     url_open.side_effect = side_effect
-
-
-def help_find_invalid_chars(company_name, line, column):  # pragma: no cover
-    from crawlcrunch.compat import url_open
-    from crawlcrunch.model import LocalFilesDir
-    from crawlcrunch.model import CrunchBaseFetcherMixin
-    root = LocalFilesDir('/tmp')
-    company = root.get(company_name)
-    resp = url_open(company.query_url())
-    s = resp.read().decode('utf-8')
-    s = CrunchBaseFetcherMixin.replace_control_chars(s)
-    splitted = s.split('\n')
-    l = splitted[line - 1]
-    c = l[column - g1]
-    print('The entire line is:\n{0!r}'.format(l))
-    print('The specific character is: {0!r}'.format(c))
-    return l, c
