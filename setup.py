731abb5fa5884563e7fd8c4880597d5e86eb3e73
Thomas Bach <thbach@students.uni-mainz.de>
2012-08-17 00:38:18 +0200

    we need sqlalchemy

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ from setuptools import setup
 
 py_version = sys.version_info[:2]
 
+install_requires = ['sqlalchemy', ]
+
 tests_require = ['mock', ]
 if py_version == (2, 6):
     tests_require.append('unittest2')
@@ -14,10 +16,11 @@ setup(name='crawlcrunch',
       version='0.1',
       description='Crawl crunchbase.com for companies information.',
       author='Thomas Bach',
+      install_requires=install_requires,
       tests_require=tests_require,
       test_suite='crawlcrunch.tests',
       entry_points={
-        'console_scripts': 
+        'console_scripts':
         ['cc_update=crawlcrunch.scripts.cc_update:main', ],
         },
       )
