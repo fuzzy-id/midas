8404175a9d2208b79746273d2c31665ad4b8ead4
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-04 14:10:39 +0200

    companies are queried by the companylist

--- crawlcrunch/tests/test_dummy_backend.py
+++ crawlcrunch/tests/test_dummy_backend.py
@@ -17,15 +17,3 @@ class DummyRootTests(unittest.TestCase):
         root = self._make_one()
         from crawlcrunch.model.local_files import CompanyList
         self.assertIsInstance(root.get('companies'), CompanyList)
-
-    def test_get_a_company_returns_instance_of_company(self):
-        root = self._make_one()
-        company = root.get('facebook')
-        from crawlcrunch.model.local_files import Company
-        self.assertIsInstance(company, Company)
-
-    def test_getting_the_same_company_returns_same_instance(self):
-        root = self._make_one()
-        c1 = root.get('a_company')
-        c2 = root.get('a_company')
-        self.assertIs(c1, c2)
