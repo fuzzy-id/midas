ce02522c76867e4117da5baf0d728f9861ecae14
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-04 13:33:48 +0200

    sorting out general things

--- crawlcrunch/model/__init__.py
+++ crawlcrunch/model/__init__.py
@@ -1,1 +1,37 @@
 # -*- coding: utf-8 -*-
+
+import logging
+import json
+
+import crawlcrunch.compat
+
+
+class CrunchBaseFetcherMixin(object):
+    """ A mixin that sums up the general way to fetch data from
+    crunchbase.
+    """
+    to_replace = ('\x00', '\x03', '\x0b', '\x0e',
+                  '\x12', '\x14', '\x1d', '\x1e', '\x1f')
+    companies_list_url = 'http://api.crunchbase.com/v/1/companies.js'
+    company_url_tpl = 'http://api.crunchbase.com/v/1/company/{0}.js'
+
+    def query_url(self):  # pragma: no cover
+        raise NotImplementedError()
+
+    @classmethod
+    def replace_control_chars(cls, s):
+        for c in cls.to_replace:
+            s = s.replace(c, '')
+        return s
+
+    def fetch(self):
+        logging.info('Fetching {0}'.format(self.name))
+        response = crawlcrunch.compat.urlopen(self.query_url())
+        content = response.read()
+        s = content.decode('utf-8')
+        # Some companies have control chars in theire description
+        # *sigh*
+        s = self.replace_control_chars(s)
+        return json.loads(s)
+
+
--- crawlcrunch/model/local_files.py
+++ crawlcrunch/model/local_files.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import json
-import logging
 import os.path
 
 from crawlcrunch.compat import GzipFile
-
-import crawlcrunch.compat
-
+from crawlcrunch.model import CrunchBaseFetcherMixin
 
 class LocalFilesRoot(object):
     """ This is the root object of all data traversal.
@@ -44,35 +41,6 @@ class ZippedJsonFile(object):
             self.data = data
         with GzipFile(self.path, 'wb') as fp:
             json.dump(self.data, fp)
-
-
-class CrunchBaseFetcherMixin(object):
-    """ A mixin that sums up the general way to fetch data from
-    crunchbase.
-    """
-    to_replace = ('\x00', '\x03', '\x0b', '\x0e',
-                  '\x12', '\x14', '\x1d', '\x1e', '\x1f')
-    companies_list_url = 'http://api.crunchbase.com/v/1/companies.js'
-    company_url_tpl = 'http://api.crunchbase.com/v/1/company/{0}.js'
-
-    def query_url(self):  # pragma: no cover
-        raise NotImplementedError()
-
-    @classmethod
-    def replace_control_chars(cls, s):
-        for c in cls.to_replace:
-            s = s.replace(c, '')
-        return s
-
-    def fetch(self):
-        logging.info('Fetching {0}'.format(self.name))
-        response = crawlcrunch.compat.urlopen(self.query_url())
-        content = response.read()
-        s = content.decode('utf-8')
-        # Some companies have control chars in theire description
-        # *sigh*
-        s = self.replace_control_chars(s)
-        return json.loads(s)
 
 
 class Company(CrunchBaseFetcherMixin):
