97008a127ad330ad63a65d9be3bb6fac026914a3
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-04 14:11:27 +0200

    reducing complexity

--- crawlcrunch/tests/__init__.py
+++ crawlcrunch/tests/__init__.py
@@ -6,37 +6,17 @@ import mock
 import os.path
 import sys
 
+from crawlcrunch.compat import PY_VERSION
 from crawlcrunch.compat import StringIO
 from crawlcrunch.compat import comp_bytes
 
-py_version = sys.version_info[:2]
-
-if py_version == (2, 6):  # pragma: no cover
+if PY_VERSION == (2, 6):  # pragma: no cover
     import unittest2 as unittest
 else:
     import unittest
 
+
 logging.basicConfig(level=logging.WARNING)
-
-
-class DummyRoot(mock.Mock):
-
-    def __init__(self, path=None):
-        from crawlcrunch.model.local_files import LocalFilesRoot
-        mock.Mock.__init__(self, spec=LocalFilesRoot)
-        self.nodes = {}
-        self.get = mock.Mock(side_effect=self._dummy_get)
-
-    def _dummy_get(self, name):
-        if self.nodes.get(name, None) is None:
-            if name == 'companies':
-                from crawlcrunch.model.local_files import CompanyList
-                m = mock.Mock(spec=CompanyList)
-            else:
-                from crawlcrunch.model.local_files import Company
-                m = mock.Mock(spec=Company)
-            self.nodes[name] = m
-        return self.nodes[name]
 
 __here__ = os.path.abspath(os.path.dirname(__file__))
 __test_examples__ = os.path.join(__here__, 'destinations')
@@ -45,6 +25,20 @@ EXAMPLES_PATH = {
     'company_files_empty': os.path.join(__test_examples__, 'company_files_empty'),
     'no_company_files': os.path.join(__test_examples__, 'no_company_files') }
 
+class DummyRoot(mock.Mock):
+
+    def __init__(self, path=None):
+        from crawlcrunch.model.local_files import LocalFilesRoot
+        mock.Mock.__init__(self, spec=LocalFilesRoot)
+        self.get = mock.Mock(side_effect=self._dummy_get)
+
+    def _dummy_get(self, name):
+        if name == 'companies':
+            from crawlcrunch.model.local_files import CompanyList
+            return mock.Mock(spec=CompanyList)
+        raise NotImplementedError("Unknown class '{0}'".format(name))
+
+
 def _make_json_buffer(obj):
     from io import BytesIO
     js = json.dumps(obj)
@@ -52,7 +46,6 @@ def _make_json_buffer(obj):
     buf = BytesIO(b)
     buf.seek(0)
     return buf
-
 
 def prepare_url_open(url_open, return_dict):
     def side_effect(url):
