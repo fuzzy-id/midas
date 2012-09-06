48dcc1c642a68f1d486016a0770e49b595a749ce
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-06 14:26:27 +0200

    decrease memory footprint

--- ev_transpose/ev_transpose/reducer.py
+++ ev_transpose/ev_transpose/reducer.py
@@ -1,27 +1,41 @@
 # -*- coding: utf-8 -*-
 
 import datetime
+import os
 import sys
 
 from ev_transpose import TP_TSTAMP_FORMAT
 from ev_transpose import Entry
 
 
+MIN_DATE = datetime.datetime(1900, 1, 1).strftime(TP_TSTAMP_FORMAT)
+ONE_DAY = datetime.timedelta(days=1)
+
 def reducer():
     cache = []
+    min_date = os.environ.get('TP_MIN_DATE', MIN_DATE)
+    min_date = datetime.datetime.strptime(min_date, TP_TSTAMP_FORMAT)
     for line in sys.stdin:
         entry = line_to_entry(line)
-        for i in range(len(cache) + 1):  # doing an insertion sort
+        if entry.date == min_date:
+            out(entry)
+            min_date += ONE_DAY
             try:
-                if entry.date < cache[i].date:
-                    cache.insert(i, entry)
-                    break
+                while cache[0].date == min_date:  # relasing resources
+                    out(cache.pop(0))
+                    min_date += ONE_DAY
             except IndexError:
-                cache.insert(i, entry)
+                pass
+        else:
+            for i in range(len(cache) + 1):  # doing an insertion sort
+                try:
+                    if entry.date < cache[i].date:
+                        cache.insert(i, entry)
+                        break
+                except IndexError:
+                    cache.insert(i, entry)
     for entry in cache:
-        date = datetime.datetime.strftime(entry.date, 
-                                          TP_TSTAMP_FORMAT)
-        print('{e.name}\t{0}, {e.rank}'.format(date, e=entry))
+        out(entry)
     return 0
 
 def line_to_entry(line):
@@ -30,3 +44,9 @@ def line_to_entry(line):
     date = datetime.datetime.strptime(date, TP_TSTAMP_FORMAT)
     rank = int(rank)
     return Entry(name=name, date=date, rank=rank)
+
+def out(entry):
+    date = datetime.datetime.strftime(entry.date, 
+                                      TP_TSTAMP_FORMAT)
+    print('{e.name}\t{0}, {e.rank}'.format(date, e=entry))
+    
--- ev_transpose/ev_transpose/tests/test_reducer.py
+++ ev_transpose/ev_transpose/tests/test_reducer.py
@@ -7,6 +7,7 @@ from ev_transpose.tests import IntegrationTestCase
 from ev_transpose.tests import unittest
 from ev_transpose import Entry
 
+import mock
 
 class HelperTests(unittest.TestCase):
 
@@ -22,8 +23,8 @@ class ScriptTests(IntegrationTestCase):
     def test_reducer(self):
         from ev_transpose.reducer import reducer
         data = ['foo\t2012-09-05, 1',
-                'foo\t2012-09-03, 1',
                 'foo\t2012-09-06, 3',
+                'foo\t2012-09-03, 1',
                 'foo\t2012-09-04, 2']
         sys.stdin.write('\n'.join(data))
         sys.stdin.seek(0)
@@ -34,7 +35,22 @@ class ScriptTests(IntegrationTestCase):
         expected = '\n'.join(data)
         self.assertEqual(out, expected)
 
+    @mock.patch.dict('os.environ', {'TP_MIN_DATE': '2012-09-03'})
+    def test_reducer_with_defined_min_date(self):
+        from ev_transpose.reducer import reducer
+        data = ['foo\t2012-09-05, 1',
+                'foo\t2012-09-06, 3',
+                'foo\t2012-09-03, 1',
+                'foo\t2012-09-04, 2']
+        sys.stdin.write('\n'.join(data))
+        sys.stdin.seek(0)
+        self.assertEqual(reducer(), 0)
+        sys.stdout.seek(0)
+        result = sys.stdout.getvalue().strip().split('\n')
+        self.assertEqual(len(result), len(data))
+        data.sort()
+        self.assertEqual(result, data)
+
 
 if __name__ == '__main__':  # pragma: no cover
     unittest.main()
-
