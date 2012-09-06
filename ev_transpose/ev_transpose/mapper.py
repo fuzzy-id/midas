bb4dc2362014e6443b058d271ab6070b1ddf414a
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-06 15:07:22 +0200

    easier to test

--- ev_transpose/ev_transpose/mapper.py
+++ ev_transpose/ev_transpose/mapper.py
@@ -10,14 +10,18 @@ from ev_transpose.compat import ZipFile
 TSTAMP_FORMAT = 'top-1m-%Y-%m-%d.csv.zip'
 
 def mapper():
+    tstamp = convert_fname_to_tstamp()
     for fname in sys.stdin:
-        fname_last = os.path.basename(fname)
-        date = datetime.datetime.strptime(fname_last, TSTAMP_FORMAT)
-        tstamp = date.strftime(TP_TSTAMP_FORMAT)
         for l in unzip_file(fname):
             rank, name = split_rank_name(l)
             print('{0}\t{1}, {2}'.format(name, tstamp, rank))
     return 0
+
+def convert_fname_to_tstamp(fname):
+    fname_last = os.path.basename(fname)
+    date = datetime.datetime.strptime(fname_last, TSTAMP_FORMAT)
+    return date.strftime(TP_TSTAMP_FORMAT)
+
 
 def unzip_file(fname, filelist=('top-1m.csv', )):
     ''' Iterates over the compressed file.
