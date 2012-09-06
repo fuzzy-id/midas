298ca7b26cd63c4a63ce7df497cf96de4b4ec6e7
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-06 12:26:20 +0200

    implementing the mapper

--- ev_transpose/ev_transpose/scripts.py
+++ ev_transpose/ev_transpose/scripts.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import datetime
+import os.path
+import sys
+
+from ev_transpose import split_rank_name
+from ev_transpose import unzip_file
+
+TSTAMP_FORMAT = 'top-1m-%Y-%m-%d.csv.zip'
+
 def mapper():
-    pass
+    for fname in sys.stdin:
+        fname_last = os.path.basename(fname)
+        date = datetime.datetime.strptime(fname_last, TSTAMP_FORMAT)
+        tstamp = date.strftime('%Y-%m-%d')
+        for l in unzip_file(fname):
+            rank, name = split_rank_name(l)
+            print('{0}\t{1}, {2}'.format(name, tstamp, rank))
+    return 0
 
 def reducer():
     pass
