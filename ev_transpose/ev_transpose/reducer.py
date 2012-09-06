a2f3e3bc7bbc68aa3bdfdb747d94ef5ff098b6e2
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-06 13:19:20 +0200

    parsing the input

--- ev_transpose/ev_transpose/reducer.py
+++ ev_transpose/ev_transpose/reducer.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from ev_transpose import TP_TSTAMP_FORMAT
+from ev_transpose import Entry
+
+import datetime
+
 def reducer():
     pass
+
+def line_to_entry(line):
+    name, tail = line.split('\t')
+    date, rank = tail.split(',')
+    date = datetime.datetime.strptime(date, TP_TSTAMP_FORMAT)
+    rank = int(rank)
+    return Entry(name=name, date=date, rank=rank)
