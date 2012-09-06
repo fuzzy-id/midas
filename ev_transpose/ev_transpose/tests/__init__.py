cdb5bd2fb976a9848d788143fffa1e5611637260
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-06 10:02:30 +0200

    what we expect…

--- ev_transpose/ev_transpose/tests/__init__.py
+++ ev_transpose/ev_transpose/tests/__init__.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+
+import os.path
+
+__here__ = os.path.abspath(os.path.dirname(__file__))
+__test_data__ = os.path.join(__here__, 'data')
+TEST_DATA = (
+    (os.path.join(__test_data__, 'top-1m-2012-09-03.csv.zip', ),
+     {'foo.csv.zip': '2012-09-03, 1',
+      'bar.csv.zip': '2012-09-03, 2'}), )
