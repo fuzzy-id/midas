e5bff15e5e344f27f4ea322dce5ad31f11fa4323
Thomas Bach <vince@vinceblog.net>
2012-09-07 14:19:00 +0200

    describing the algorithm

--- ev_transpose/ev_transpose/script.py
+++ ev_transpose/ev_transpose/script.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import os.path
 import sys
 
+from ev_transpose import TP_TSTAMP_FORMAT
 
 def main(argv=sys.argv):
     command = EvTranspose(argv)
@@ -17,6 +19,22 @@ class EvTranspose(object):
     
     def __init__(self, argv):
         self.args = self.parser.parse_args(argv[1:])
+        self.dst = self.args.dst
 
     def run(self):
+        for zfile in self.args.zip_file:
+            with InputFile(zfile) as in_f:
+                for entry in in_f:
+                    self.write_out(entry)
         return 0
+
+    def write_out(self, entry):
+        with GzipFile(self.expand(entry.name), 'w') as fp:
+            fp.write(self.format_out(entry))
+
+    def expand(self, fname):
+        return os.path.join(self.dst, '.'.join((fname, 'gz')))
+
+    def format_out(self, entry):
+        date = entry.date.strftime(TP_TSTAMP_FORMAT)
+        return '{0}, {1}'.format(date, entry.rank)
