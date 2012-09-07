82f8a55f591c59a0a3981ff4ac7571dedc4ea740
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-07 10:39:54 +0200

    scaffold for resulting script

---
+++ ev_transpose/ev_transpose/script.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+
+def main(argv=sys.argv):
+    command = EvTranspose(argv)
+    return command.run()
+
+class EvTranspose(object):
+
+    description = ""
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('dst', help='the destination for the output files')
+    parser.add_argument('zip_file', help='the zipped files to parse')
+    
+    def __init__(self, argv):
+        self.args = self.parser.parse_args(argv[1:])
+
+    def run(self):
+        return 0
