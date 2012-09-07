18b744af944e01ffc5c2400445876f87e2de22fc
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-07 11:17:04 +0200

    defined the behavior of our script

--- ev_transpose/ev_transpose/tests/test_script.py
+++ ev_transpose/ev_transpose/tests/test_script.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import os
+import shutil
 import sys
+import tempfile
 
 from ev_transpose.compat import StringIO
+from ev_transpose.tests import TEST_DATA 
 from ev_transpose.tests import unittest
 
 class PatchedStderrTestCase(unittest.TestCase):
@@ -51,6 +55,31 @@ class ArgParserTests(PatchedStderrTestCase):
         args = self._make_one('foo', 'bar', 'baz')
         self.assertEqual(args.dst, 'foo')
         self.assertEqual(args.zip_file, ['bar', 'baz'])
+
+class EvTransposeTests(PatchedStderrTestCase):
+
+    def setUp(self):
+        self.tmpd = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tmpd)
+
+    def _make_one(self, *args):
+        from ev_transpose.script import EvTranspose
+        effargs = ['ev_transpose']
+        effargs.extend(args)
+        return EvTranspose(effargs)
+
+    def test_on_test_data_one(self):
+        cmd = self._make_one(self.tmpd, *TEST_DATA[0][0])
+        self.assertEqual(cmd.run(), 0)
+        dir_listing = os.listdir(self.tmpd)
+        dir_listing.sort()
+        self.assertEqual(dir_listing, ['bar.gz', 'foo.gz'])
+        with GzipFile(os.path.join(self.tmpd, 'bar.gz')) as fp:
+            self.assertEqual(fp.readlines(), TEST_DATA[0][1]['bar'])
+        with GzipFile(os.path.join(self.tmpd, 'foo.gz')) as fp:
+            self.assertEqual(fp.readlines(), TEST_DATA[0][1]['foo'])
 
 class IntegrationTests(PatchedStderrTestCase):
 
