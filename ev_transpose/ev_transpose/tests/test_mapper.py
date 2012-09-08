8dae834fd1626d160c70786f4e9462460e929da5
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-08 08:00:39 +0200

    putting things together

--- ev_transpose/ev_transpose/tests/test_mapper.py
+++ ev_transpose/ev_transpose/tests/test_mapper.py
@@ -7,22 +7,3 @@ from ev_transpose.tests import IntegrationTestCase
 from ev_transpose.tests import unittest
 
 
-class HelperTests(unittest.TestCase):
-
-    def test_unzipping_file(self):
-        from ev_transpose.script import unzip_file
-        result = list(unzip_file(TEST_DATA[0][0]))
-        expected = [ '{e.rank},{e.name}'.format(e=entry)
-                     for entry in TEST_DATA[0][1] ]
-        self.assertEqual(result, expected)
-
-    def test_parse_line(self):
-        from ev_transpose.script import split_rank_name
-        result = split_rank_name('1,foo')
-        self.assertEqual(result, (1, 'foo'))
-
-    def test_parse_line_w_comma_in_name(self):
-        ' This seems to happen. *sigh* '
-        from ev_transpose.script import split_rank_name
-        result = split_rank_name('1,foo,bar')
-        self.assertEqual(result, (1, 'foo,bar'))
--- ev_transpose/ev_transpose/tests/test_script.py
+++ ev_transpose/ev_transpose/tests/test_script.py
@@ -89,6 +89,27 @@ class EvTransposeTests(unittest.TestCase):
                          'example.com, 1900-01-01, 2')
 
 
+class HelperTests(unittest.TestCase):
+
+    def test_unzipping_file(self):
+        from ev_transpose.script import unzip_file
+        result = list(unzip_file(TEST_DATA[0][0]))
+        expected = [ '{e.rank},{e.name}'.format(e=entry)
+                     for entry in TEST_DATA[0][1] ]
+        self.assertEqual(result, expected)
+
+    def test_parse_line(self):
+        from ev_transpose.script import split_rank_name
+        result = split_rank_name('1,foo')
+        self.assertEqual(result, (1, 'foo'))
+
+    def test_parse_line_w_comma_in_name(self):
+        ' This seems to happen. *sigh* '
+        from ev_transpose.script import split_rank_name
+        result = split_rank_name('1,foo,bar')
+        self.assertEqual(result, (1, 'foo,bar'))
+
+
 class IntegrationTests(PatchedStderrTestCase):
 
     def setUp(self):
