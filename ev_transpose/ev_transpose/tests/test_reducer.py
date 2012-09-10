2109f8fde085bbacd47ea47c47bb231b6cc06cf5
Thomas Bach <vince@vinceblog.net>
2012-09-10 19:01:32 +0200

    we want the data sorted, by date

--- ev_transpose/ev_transpose/tests/test_reducer.py
+++ ev_transpose/ev_transpose/tests/test_reducer.py
@@ -31,25 +31,9 @@ class ScriptTests(IntegrationTestCase):
         self.assertEqual(reducer(), 0)
         sys.stdout.seek(0)
         out = sys.stdout.getvalue().strip()
-        #data.sort()
+        data.sort()
         expected = '\n'.join(data)
         self.assertEqual(out, expected)
-
-    @mock.patch.dict('os.environ', {'TP_MIN_DATE': '2012-09-03'})
-    def test_reducer_with_defined_min_date(self):
-        from ev_transpose.reducer import reducer
-        data = ['foo\t2012-09-05, 1',
-                'foo\t2012-09-06, 3',
-                'foo\t2012-09-03, 1',
-                'foo\t2012-09-04, 2']
-        sys.stdin.write('\n'.join(data))
-        sys.stdin.seek(0)
-        self.assertEqual(reducer(), 0)
-        sys.stdout.seek(0)
-        result = sys.stdout.getvalue().strip().split('\n')
-        self.assertEqual(len(result), len(data))
-        #data.sort()
-        self.assertEqual(result, data)
 
 
 if __name__ == '__main__':  # pragma: no cover
