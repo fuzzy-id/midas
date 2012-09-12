cda5a27bac43ad69ec6729acc1e7cacf1afaa833
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-12 18:47:34 +0200

    get the version from hbase

--- hbase/hbase/tests.py
+++ hbase/hbase/tests.py
@@ -21,3 +21,13 @@ class TableListTests(unittest.TestCase):
         urlopen().readall.return_value = b'{"table":[{"name":"test"}]}'
         hb_con = self._make_one()
         self.assertEqual(list(hb_con.get_tables()), ['test'])
+
+    @mock.patch("hbase.compat.urlopen")
+    def test_query_version_information(self, urlopen):
+        urlopen().readall.return_value = b'{"JVM":"Oracle Corporation 1.7.0_07-23.3-b01","Jersey":"1.8","OS":"Linux 3.4.2-x86_64-linode25 amd64","REST":"0.0.2","Server":"jetty/6.1.26"}'
+        hb_con = self._make_one()
+        self.assertEqual(hb_con.get_version(), {"JVM": "Oracle Corporation 1.7.0_07-23.3-b01",
+                                                "Jersey": "1.8",
+                                                "OS": "Linux 3.4.2-x86_64-linode25 amd64",
+                                                "REST": "0.0.2",
+                                                "Server": "jetty/6.1.26"})
