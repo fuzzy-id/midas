fefab5f18cdd166e9abab5d5652cdc7645f2d6ae
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-14 08:28:54 +0200

    Revert "don't know how to test these things"
    This reverts commit a3e1d48b426000bc182265db336a907f3df4996d.

--- hbase/setup.py
+++ hbase/setup.py
@@ -2,6 +2,17 @@
 
 from setuptools import setup
 
+import sys
+
+PY_VERSION = sys.version_info[:2]
+
+tests_require = ['mock']
+
+if PY_VERSION == (2, 6):
+    tests_require.append('unittest2')
+
 setup(name='hbase',
       version='0.0a1',
-      author='Thomas Bach')
+      author='Thomas Bach',
+      tests_require=tests_require,
+      test_suite='hbase.tests')
