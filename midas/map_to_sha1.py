206a3b3ff8dc36ffb695d6ace4e060beeb673635
vince@taris.box <vince@taris.box>
2012-09-19 14:35:48 +0200

    merging scripts to transform the data

--- midas/map_to_sha1.py
+++ midas/map_to_sha1.py
@@ -2,27 +2,8 @@
 
 from __future__ import print_function
 
-import datetime
-import hashlib
-import os.path
 import sys
 
-from midas import TP_TSTAMP_FORMAT
-from midas.compat import ZipFile
-
-TSTAMP_FORMAT = 'top-1m-%Y-%m-%d.csv.zip'
-
-def mapper():
-    for fname in sys.stdin:
-        fname = fname.strip()
-        print("Processing '{0}'".format(fname), file=sys.stderr)
-        tstamp = convert_fname_to_tstamp(fname)
-        for l in unzip_file(fname):
-            rank, name = split_rank_name(l)
-            h = hashlib.sha1(name).hexdigest()
-            h_start = h[:2]
-            print("{0}\t{1}, {2}, {3}".format(h_start, name, tstamp, rank))
-    return 0
 
 def normalize_site(name):
     splits = name.split('/')
@@ -34,22 +15,3 @@ def normalize_site(name):
     splits = host.split('.')
     return '.'.join(splits[-2:])
 
-def convert_fname_to_tstamp(fname):
-    date = convert_fname_to_date(fname)
-    return date.strftime(TP_TSTAMP_FORMAT)
-
-def convert_fname_to_date(fname):
-    fname_last = os.path.basename(fname)
-    return datetime.datetime.strptime(fname_last, TSTAMP_FORMAT)
-
-def unzip_file(fname, filelist=('top-1m.csv', )):
-    ''' Iterates over the compressed file.
-    '''
-    with ZipFile(fname) as zf:
-        for zipped_file in filelist:
-            for line in zf.open(zipped_file):
-                yield line.decode().strip()
-
-def split_rank_name(line):
-    rank, name = line.split(',', 1)
-    return int(rank), name
--- midas/transform.py
+++ midas/transform.py
@@ -2,6 +2,9 @@
 
 from __future__ import print_function
 
+import datetime
+import hashlib
+import os.path
 import sys
 
 from sqlalchemy import Column
@@ -12,10 +15,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from midas.mapper import convert_fname_to_date
-from midas.mapper import split_rank_name
-from midas.mapper import unzip_file
-
+from midas import TP_TSTAMP_FORMAT
+from midas.compat import ZipFile
 
 Base = declarative_base()
 Session = sessionmaker()
@@ -28,6 +29,20 @@ class Rank(Base):
     rank = Column(Integer)
     ts = Column(DateTime)
 
+
+TSTAMP_FORMAT = 'top-1m-%Y-%m-%d.csv.zip'
+
+def mapper():
+    for fname in sys.stdin:
+        fname = fname.strip()
+        print("Processing '{0}'".format(fname), file=sys.stderr)
+        tstamp = convert_fname_to_tstamp(fname)
+        for l in unzip_file(fname):
+            rank, name = split_rank_name(l)
+            h = hashlib.sha1(name).hexdigest()
+            h_start = h[:2]
+            print("{0}\t{1}, {2}, {3}".format(h_start, name, tstamp, rank))
+    return 0
 
 def push_to_db():
     db = sys.argv[1]
@@ -48,3 +63,23 @@ def push_to_db():
             session.commit()
             print("Commited", file=sys.stderr)
     return 0
+
+def convert_fname_to_tstamp(fname):
+    date = convert_fname_to_date(fname)
+    return date.strftime(TP_TSTAMP_FORMAT)
+
+def convert_fname_to_date(fname):
+    fname_last = os.path.basename(fname)
+    return datetime.datetime.strptime(fname_last, TSTAMP_FORMAT)
+
+def unzip_file(fname, filelist=('top-1m.csv', )):
+    ''' Iterates over the compressed file.
+    '''
+    with ZipFile(fname) as zf:
+        for zipped_file in filelist:
+            for line in zf.open(zipped_file):
+                yield line.decode().strip()
+
+def split_rank_name(line):
+    rank, name = line.split(',', 1)
+    return int(rank), name
