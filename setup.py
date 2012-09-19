84d9941d41e5e63429db241f0ac3c4be7ecafbd2
vince@taris.box <vince@taris.box>
2012-09-19 14:04:11 +0200

    proper naming

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ tests_require = ['mock']
 if PY_VERSION == (2, 6):
     tests_require.append('unittest2')
 
-setup(name='ev_transpose',
+setup(name='midas',
       version='0.0a1',
-      description='Transpose time-series gathered over csv-files.',
+      description='Try to get some useful information from Top1M.',
       author='Thomas Bach',
       tests_require=tests_require,
-      test_suite='ev_transpose.tests',
+      test_suite='midas.tests',
       entry_points={
         'console_scripts':
-            ['ev_top1m_to_sha1=ev_transpose.mapper:mapper',
-             'ev_alex2db=ev_transpose.model:push_to_db']
+            ['md_top1m_to_sha1=midas.mapper:mapper',
+             'md_top1m_to_db=midas.model:push_to_db']
         })
