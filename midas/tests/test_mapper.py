630cbd8eb7b2afc5fdcc777ad10d37cfd223dc0e
vince@taris.box <vince@taris.box>
2012-09-19 14:21:27 +0200

    function is not needed, yet

--- midas/tests/test_mapper.py
+++ midas/tests/test_mapper.py
@@ -2,9 +2,9 @@
 
 import sys
 
-from ev_transpose.tests import TEST_DATA
-from ev_transpose.tests import IntegrationTestCase
-from ev_transpose.tests import unittest
+from midas.tests import TEST_DATA
+from midas.tests import IntegrationTestCase
+from midas.tests import unittest
 
 
 class HelperTests(unittest.TestCase):
@@ -26,9 +26,3 @@ class HelperTests(unittest.TestCase):
         from ev_transpose.mapper import split_rank_name
         result = split_rank_name('1,foo,bar')
         self.assertEqual(result, (1, 'foo,bar'))
-
-    def test_normalize_name(self):
-        from ev_transpose.mapper import normalize_site
-        self.assertEqual('example.com', normalize_site('example.com'))
-        self.assertEqual('example.com', normalize_site('http://www.example.com/'))
-        self.assertEqual('example.com', normalize_site('www.example.com/foo'))
