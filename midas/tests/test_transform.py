7ed6848044dcb8c105116ec38b890c557a638d79
vince@taris.box <vince@taris.box>
2012-09-19 14:37:48 +0200

    consistent naming

--- midas/tests/test_transform.py
+++ midas/tests/test_transform.py
@@ -10,19 +10,19 @@ from midas.tests import unittest
 class HelperTests(unittest.TestCase):
 
     def test_unzipping_file(self):
-        from ev_transpose.mapper import unzip_file
+        from ev_transpose.transform import unzip_file
         result = list(unzip_file(TEST_DATA[0][0]))
         expected = [ '{e.rank},{e.name}'.format(e=entry)
                      for entry in TEST_DATA[0][1] ]
         self.assertEqual(result, expected)
 
     def test_parse_line(self):
-        from ev_transpose.mapper import split_rank_name
+        from ev_transpose.transform import split_rank_name
         result = split_rank_name('1,foo')
         self.assertEqual(result, (1, 'foo'))
 
     def test_parse_line_w_comma_in_name(self):
         ' This seems to happen. *sigh* '
-        from ev_transpose.mapper import split_rank_name
+        from ev_transpose.transform import split_rank_name
         result = split_rank_name('1,foo,bar')
         self.assertEqual(result, (1, 'foo,bar'))
