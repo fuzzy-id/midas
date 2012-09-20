473e723f630973d0d20a5b46019ed39f61935e6e
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-20 10:40:39 +0200

    find names which need speciat treatment

---
+++ midas/assign.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Assigns the names found in top1m data an entry in the CrunchBase data.
+"""
+
+import sys
+
+def find_strange_names():
+    """ Finds names which have special characters in their names.  Can
+    run in Hadoop.
+    """
+    norm_chars = set( chr(i) for i in range(ord('a'), ord('z') + 1) )
+    norm_chars.update( chr(i) for i in range(ord('A'), ord('Z') + 1) )
+    norm_chars.update( str(i) for i in range(0, 10) )
+    norm_chars.add('.')
+    for line in sys.stdin:
+        name, tail = line.split('\t')
+        for c in name:
+            if c not in norm_chars:
+                print(name)
+                break
+    return 0
