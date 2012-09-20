9353dc2be59eda312ee094b9198a1bdaa99926f1
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-20 12:52:49 +0200

    defining common ways to parse and write out entries

--- midas/__init__.py
+++ midas/__init__.py
@@ -2,5 +2,18 @@
 
 import collections
 
-TP_TSTAMP_FORMAT = '%Y-%m-%d'
 Entry = collections.namedtuple('Entry', ['name', 'date', 'rank'])
+
+class Entry(object):
+
+    TS_FORMAT = '%Y-%m-%d'
+
+    def __init__(name, date, rank):
+        self.name = name
+        self.date = date
+        self.rank = rank
+
+    def __str__(self):
+        " The standard format. "
+        date = datetime.strftime(self.date, TS_FORMAT)
+        return '{e.name}\t{0}, {e.rank}'.format(date, e=self)
