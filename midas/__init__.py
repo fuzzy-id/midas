cedadaf2cc4ce10a750a8b3aab0343b06bf2beca
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-20 15:07:19 +0200

    lots of comments

--- midas/__init__.py
+++ midas/__init__.py
@@ -1,12 +1,29 @@
 # -*- coding: utf-8 -*-
+""" Common functions that are needed in most other submodules.
+"""
 
 from midas.compat import ZipFile
 
+#: How much letters of the hash key should be used to pre-sort the
+#: entries. The larger this number the more and smaller files
+#: will be produced. The default value is `2` which gives us `16 * 16 =
+#: 256` files.
 CUT_HASH_KEY = 2
 
-class Entry(object):
+class RankEntry(object):
+    """ Returns an entry of a ranking for `site`.
 
+    The `date` specifies the date of the entry and should be a
+    :class:`datetime.datetime` instance.
+
+    The `rank` is expected to be an integer.
+
+    Methods are provided both, to format a :class:`RankEntry` in
+    standardized manners and to parse these formats back.
+    """ 
+    #: The standard format we use to produce and parse time-stamps.
     TS_FORMAT = '%Y-%m-%d'
+    #: The time-stamp format as encoded in Alexa's Top1M files.
     ALEXA_TS_FORMAT = 'top-1m-%Y-%m-%d.csv.zip'
 
     def __init__(self, name, date, rank):
@@ -15,27 +32,42 @@ class Entry(object):
         self.rank = rank
         self._key = None
 
+    @property
     def format_std(self):
-        " The standard format: ``NAME\tTSTAMP, RANK``. "
-        date = datetime.strftime(self.date, TS_FORMAT)
-        return '{e.name}\t{0}, {e.rank}'.format(date, e=self)
+        " `'name[TAB]tstamp,[SPACE]rank'`. "
+        return '{e.name}\t{e.tstamp}, {e.rank}'.format(e=self)
 
+    @property
+    def tstamp(self):
+        " The `date` formated as defined in :attr:`TS_FORMAT`. "
+        return datetime.strftime(self.date, self.TS_FORMAT)
+
+    @property
     def format_w_key(self):
-        " ``KEY\tSTD_FORMAT`` "
-        return '{0}\t{1}'.format(self.key, self.format_std())
+        " `'key[TAB]format_std'` "
+        return '{e.key}\t{e.format_std}'.format(e=self)
 
     @property
     def key(self):
+        """ The first :attr:`CUT_HASH_KEY` digits of the hash produced
+        by :func:`hashlib.sha1` on the `name`. """
         if self._key is None:
             self._key = hashlib.sha1(self.name).hexdigest()[:CUT_HASH_KEY]
         return self._key
         
-
     @classmethod
     def iter_alexa_file(cls, fname):
-        """ In the standard Alexa Top1M data the time-stamp is encoded
-        in the filename. Hence, parsing cannot be done line-by-line
-        but per file as we need the time-stamp.
+        """ The standard Alexa Top1M data is a
+        :class:`zipfile.ZipFile`. The archive contains one file named
+        `top-1m.csv`. The lines in this file have the form
+        `rank,site_name` (no spaces!).
+
+        As the time-stamp is encoded in the file name (defined by
+        :attr:`ALEXA_TS_FORMAT`) and not in the line, parsing cannot be
+        done line-by-line but needs to be done on a per file basis.
+
+        Returns an iterator yielding a :class:`RankEntry` for every
+        line it finds in the file in the archive.
         """
         fname_last = os.path.basename(fname)
         date = datetime.datetime.strptime(fname_last, cls.ALEXA_TS_FORMAT)
