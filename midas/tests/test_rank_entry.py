82b0f9809675b7c92576a279c1240ed7afd2305a
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-20 21:09:26 +0200

    gardening

--- midas/tests/test_rank_entry.py
+++ midas/tests/test_rank_entry.py
@@ -13,6 +13,7 @@ TEST_DATA = (os.path.join(__test_data__, 'top-1m-2012-09-03.csv.zip'),
              (RankEntry('foo', datetime.datetime(2012, 9, 3), 1),
               RankEntry('bar', datetime.datetime(2012, 9, 3), 2)))
 
+
 class RankEntryTestCase(unittest.TestCase):
 
     def _get_target_cls(self):
@@ -32,6 +33,7 @@ class FormatTests(RankEntryTestCase):
     def test_format_w_key(self):
         entry = self._make_one('foo', datetime.datetime(1900, 1, 1), 1)
         self.assertEqual(entry.format_w_key, '0b\tfoo\t1900-01-01, 1')
+
 
 class IterAlexaFileTests(RankEntryTestCase):
 
