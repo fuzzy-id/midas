5824d3398482b0a635aec008b1446f86b0755ad0
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-20 21:19:48 +0200

    test on provided test data

--- midas/tests/test_transform.py
+++ midas/tests/test_transform.py
@@ -5,6 +5,7 @@ import sys
 import mock
 
 from midas.compat import StringIO
+from midas.tests import TEST_DATA
 from midas.tests import IntegrationTestCase
 
 class AlexaToSha1Tests(IntegrationTestCase):
@@ -20,3 +21,9 @@ class AlexaToSha1Tests(IntegrationTestCase):
             self._run('-h')
         self.assertEqual(cm.exception.code, 0)
         self.assert_stdout_startswith('usage: ')
+
+    def test_on_test_data(self):
+        ret_code = self._run('-q', TEST_DATA[0])
+        self.assertEqual(ret_code, 0)
+        self.assert_stdout_equal('\n'.join(e.format_w_key 
+                                           for e in TEST_DATA[1]) + '\n')
