37dd5f5a3abf034ef39bde83d74ed832ab955692
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-25 19:51:23 +0200

    corrected algo

--- midas/model.py
+++ midas/model.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import itertools
+import os.path
+
 from midas import RankEntry
 from midas.compat import GzipFile
 
 def lookup_ranking(name, key_dir):
     key = RankEntry.make_key(name)
-    fname = os.path.join(name, key_dir)
+    fname = os.path.join(key_dir, '{0}.gz'.format(key))
     entries = []
     with GzipFile(fname) as fp:
+        itertools.dropwhile(lambda l: RankEntry.parse_std(l).name != name, fp)
         for line in fp:
-            entry = RankEntry.parse_key(line)
+            entry = RankEntry.parse_std(line)
             if entry.name == name:
-                entries.append(entry)
-    return entries
+                yield entry
+            else:
+                break
