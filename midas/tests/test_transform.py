5e680f7a0e760c15ad51fad35a93639ef893bedd
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-26 08:58:51 +0200

    we want all elements sorted in the file

--- midas/tests/test_transform.py
+++ midas/tests/test_transform.py
@@ -2,6 +2,7 @@
 
 import datetime
 import os
+import random
 import shutil
 import sys
 import tempfile
@@ -9,6 +10,7 @@ import tempfile
 import mock
 
 from midas import RankEntry
+from midas.compat import GzipFile
 from midas.compat import StringIO
 from midas.tests import TEST_DATA
 from midas.tests import IntegrationTestCase
@@ -47,10 +49,17 @@ class KeyToFilesTests(IntegrationTestCase):
     @mock.patch('midas.transform.get_hadoop_binary')
     def test_on_test_data(self, hadoop_bin):
         a_date = datetime.date(1900, 1, 1)
-        entries = (RankEntry('foo', a_date, 1),
-                   RankEntry('foo', a_date, 2),
-                   RankEntry('bar', a_date, 1),
-                   RankEntry('bar', a_date, 2))
+        one_day = datetime.timedelta(days=1)
+        foos = (RankEntry('foo', a_date, 2),
+                RankEntry('foo', a_date + one_day, 1),
+                RankEntry('foo', a_date + one_day*2, 1),
+                RankEntry('foo', a_date + one_day*3, 1))
+        bars = (RankEntry('bar', a_date, 2),
+                RankEntry('bar', a_date + one_day, 1),
+                RankEntry('bar', a_date + one_day*2, 1),
+                RankEntry('bar', a_date + one_day*5, 1))
+        data = list(foos) + list(bars)
+        random.shuffle(data)
         hadoop_bin.return_value = 'echo'
         tmpd = tempfile.mkdtemp()
         try:
@@ -58,12 +67,16 @@ class KeyToFilesTests(IntegrationTestCase):
                 mkdtemp.return_value = tmpd
                 with mock.patch('midas.transform.shutil.rmtree') as rmtree:
                     ret_code = self._run('-q', '-d', tmpd, 
-                                         *( e.format_w_key for e in entries ))
+                                         *( e.format_w_key for e in data ))
                     rmtree.assert_called_with(tmpd)
             self.assertEqual(ret_code, 0)
             listing = os.listdir(tmpd)
             listing.sort()
             # foo -> 0b; bar -> 62
             self.assertEqual(listing, ['0b.gz', '62.gz']) 
+            with GzipFile(os.path.join(tmpd, '0b.gz')) as fp:
+                self.assertEqual(fp.readlines(), [ l.format_std + '\n' for l in foos ])
+            with GzipFile(os.path.join(tmpd, '62.gz')) as fp:
+                self.assertEqual(fp.readlines(), [ l.format_std + '\n' for l in bars ])
         finally:
             shutil.rmtree(tmpd)
