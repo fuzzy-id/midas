8420f184449c0325b78d56ca49969c4476dd6daa
Thomas Bach <thbach@students.uni-mainz.de>
2012-09-29 18:56:21 +0200

    added scripts

--- setup.py
+++ setup.py
@@ -27,4 +27,6 @@ setup(name='midas',
         'console_scripts':
             ['md_alexa_to_key=midas.transform:run_alexa_to_key',
              'md_key_to_files=midas.transform:run_key_to_files',
+             'md_alexa_to_names_and_one=midas.analyze:run_alexa_to_names_and_one',
+             'md_sum_values=midas.analyze:run_sum_names',
              'md_find_strange_names=midas.assign:find_strange_names']})
