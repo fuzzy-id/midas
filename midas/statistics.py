1722703d3d48ff932fabd951cab692303d2fba0e
Thomas Bach <thbach@students.uni-mainz.de>
2012-10-01 11:16:59 +0200

    some functions…

--- midas/statistics.py
+++ midas/statistics.py
@@ -2,3 +2,23 @@
 """ Code to find out about common things in both the crunchbase data
 and the Top1M data.
 """
+
+import crawlcrunch.model.db as ccdb
+
+def hps(sess):
+    return [ i[0] for i in sess.query(ccdb.Company.homepage_url)\
+                 .filter(ccdb.Company.homepage_url != None)\
+                 .filter(ccdb.Company.homepage_url != '').all() ]
+
+def netloc(url):
+    return urlparse(url).netloc
+
+def num_companies(sess):
+    return sess.query(ccdb.Company).count()
+
+def common_starts(iterable):
+    counter = collections.defaultdict(int)
+    for item in iterable:
+        start = item.split('.', 1)[0]
+        counter[start] += 1
+    return counter
