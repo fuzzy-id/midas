5cbf644e407bc163102decee8fa289b122f4616c
Thomas Bach <thbach@students.uni-mainz.de>
2012-10-05 19:40:37 +0200

    test doesn't belong here

--- midas/tests/test_analyze.py
+++ midas/tests/test_analyze.py
@@ -31,19 +31,3 @@ class SumValues(IntegrationTestCase):
                                   for name in ['foo'] * 5 + ['bar'] * 3))
         self.assertEqual(ret_code, 0)
         self.assert_stdout_equal('foo\t5\nbar\t3\n')
-
-class GetInBlocksTests(unittest.TestCase):
-
-    def _run_it(self, iterable):
-        from midas.analyze import group_by_key
-        return group_by_key(iterable)
-
-    def test_general_case(self):
-        data = ('{0}\t3'.format(e) for e in ['foo'] * 5 + ['bar'] * 3)
-        result = [ list(block) for block in self._run_it(data) ]
-        self.assertEqual(result, [['foo\t3']*5, ['bar\t3']*3])
-        
-    def test_single_key(self):
-        result = [ list(block) for block in self._run_it(['foo\t3']*1) ]
-        self.assertEqual(result, [['foo\t3']*1])
-                   
