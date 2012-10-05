36ccd8e77369169d29652fdb10791feea9cdca7d
Thomas Bach <thbach@students.uni-mainz.de>
2012-10-05 18:43:07 +0200

    getting the count

--- midas/statistics.py
+++ midas/statistics.py
@@ -9,6 +9,16 @@ from midas.compat import urlparse
 
 import crawlcrunch.model.db as ccdb
 
+SiteCnt = collections.namedtuple('NameCount', ['site', 'cnt'])
+
+def names(path):
+    with GzipFile(path) as fp:
+        for l in fp:
+            site, cnt = l.decode().strip().split('\t', 1)
+            cnt = int(cnt)
+            yield SiteCnt(site, cnt)
+            
+
 def hps(sess):
     return [ i[0] for i in sess.query(ccdb.Company.homepage_url)\
                  .filter(ccdb.Company.homepage_url != None)\
