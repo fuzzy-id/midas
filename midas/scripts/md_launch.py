53036af11b5f8b9edcea5b5780b2473983d6f102
Thomas Bach <thbach@students.uni-mainz.de>
2012-10-05 11:05:42 +0200

    we assume absolute paths

--- midas/scripts/md_launch.py
+++ midas/scripts/md_launch.py
@@ -9,19 +9,6 @@ from midas import MDCommand
 
 logger = logging.getLogger(__name__)
 
-def which(program):
-   def is_exe(fpath):
-      return (not os.path.isdir(fpath) 
-              and os.path.exists(fpath) 
-              and os.access(fpath, os.F_OK | os.X_OK))
-   if is_exe(program):
-       return program
-   for path in os.environ["PATH"].split(os.pathsep):
-       exe_file = os.path.join(path, program)
-       if is_exe(exe_file):
-           return exe_file
-   return None
-
 
 class MDLaunch(MDCommand):
     """ Launches a MapReduce job defined by the passed job
