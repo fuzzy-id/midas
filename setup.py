1d2df6e842374d3de97f40bdd417db2fbf9a3c01
Thomas Bach <thbach@students.uni-mainz.de>
2012-10-12 23:27:13 +0200

    we provide a package

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if py_version == (2, 6):
 
 setup(name='crawlcrunch',
       version='0.2',
-      provides='crawlcrunch (0.2)',
+      packages=['crawlcrunch']
       description='Crawl crunchbase.com for companies information.',
       author='Thomas Bach',
       install_requires=install_requires,
