ba74a3921ea371cc4777ec584f00cbf13d16d7ec
Thomas Bach <thbach@students.uni-mainz.de>
2012-10-13 01:03:29 +0200

    ended refactoring

--- midas/correlate.py
+++ midas/correlate.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
+""" Provides ways to associate CrunchBase data and Alexa Top1M
+sites. The main work is done via an :class:`AssociationTree` instance.
+
+The idea is to built a tree from either the domain part of
+:meth:`crawlcrunch.model.db.Company.homepage_url` or the domain part
+of the Alexa Top1M sites . 
+"""
+
 
 import collections
 
 import midas.tools as md_tools
 import midas.statistics as md_stats
 
-class BucketTree(dict):
+class AssociationTree(dict):
 
     def __init__(self, splitfunc, name='root'):
         dict.__init__(self)
         self.splitfunc = splitfunc
         self.name = name
         self.leafs = []
-        self.bucket = collections.defaultdict(list)
 
     def grow(self, item, key):
         if key is None:
@@ -20,20 +27,10 @@ class BucketTree(dict):
             return
         head, tail = self._filled_split(key)
         if head not in self:
-            self[head] = BucketTree(self.splitfunc, head)
+            self[head] = AssociationTree(self.splitfunc, head)
         self[head].grow(item, tail)
 
-    def fill(self, item, key):
-        if key is None:
-            self.bucket[None].append(item)
-            return
-        head, tail = self._filled_split(key)
-        if head not in self:
-            self.bucket[head].append(item)
-        else:
-            self[head].fill(item, tail)
-
-    def relate(self, key):
+    def associate(self, key):
         if key is None:
             if len(self.leafs) > 0:  # definite relation
                 return self.leafs
@@ -61,58 +58,45 @@ class BucketTree(dict):
             for r in branch.query(constraint):
                 yield r
 
+    def map(self, iterable, extract_func=None):
+        if extract_func is None:
+            extract_func = lambda a: a
+        result = dict()
+        for item in iterable:
+            leafs = self.associate(extract_func(item))
+            if leafs is not None:
+                result[item] = leafs
+        return result
+
+def associate_companies_to_sites(tree=None, comps=None, sites=None):
+    if tree is None:
+        tree = grow_tree_from_companies(comps)
+    if sites is None:
+        sites = md_tools.iter_interesting_sites()
+    return tree.map(sites, md_tools.domain)
+
+def associate_sites_to_companies(tree=None, sites=None, comps=None):
+    if tree is None:
+        tree = grow_tree_from_sites(sites)
+    if comps is None:
+        comps = md_tools.iter_interesting_companies()
+    return tree.map(sites, md_tools.domain)
+
+def grow_tree_from_companies(comps=None):
+    if comps is None:
+        comps = md_stats.iter_interesting_companies()
+    return grow_tree_from_sites_or_companies(comps)
+
+def grow_tree_from_sites(sites=None):
+    if sites is None:
+        sites = md_stats.iter_sites_of_interest()
+    return grow_tree_from_sites_or_companies(sites)
+
+def grow_tree_from_sites_or_companies(iterable):
+    tree = AssociationTree(split_domain)
+    for item in iterable:
+        tree.grow(item, md_tools.domain(item))
+    return tree
+
 def split_domain(site):
     return tuple(reversed(site.rsplit('.', 1)))
-
-def get_all_urls(company):
-    if company.homepage_url is not None and len(company.homepage_url) > 0:
-        yield company.homepage_url
-
-def companies_grown_tree(comps=None):
-    if comps is None:
-        comps = md_stats.companies_of_interest()
-    tree = BucketTree(domain_split_func)
-    for c in comps:
-        for url in get_all_urls(c):
-            tree.grow(c, md_tools.netloc(url).lower())
-    return tree
-
-def sites_grown_tree(sites=None):
-    if sites is None:
-        sites = md_stats.sites_of_interest()
-    tree = BucketTree(domain_split_func)
-    for site in sites:
-        domain = site_domain(site)
-        tree.grow(site, domain)
-    return tree
-
-def relate_with_sites(tree, sites=None):
-    if sites is None:
-        sites = md_stats.get_interesting_sites()
-    result = dict()
-    for s in sites:
-        l = tree.relate(site_domain(s))
-        if l is not None:
-            result[s] = l
-    return result
-
-def relate_with_companies(tree, companies=None):
-    if companies is None:
-        companies = md_stats.companies_of_interest()
-    result = dict()
-    for company in companies:
-        for url in get_all_urls(company):
-            l = tree.relate(md_tools.netloc(url).lower())
-            if l is not None:
-                result[url] = l
-    return result
-
-def fill_with_sites(tree, sites=None):
-    if sites is None:
-        sites = md_stats.get_sites()
-    for site in sites:
-        domain = site_domain(site)
-        tree.fill(site, domain)
-
-def site_domain(site):
-    return site.split('/', 1)[0].lower()
