215d1e62f0f8107600af3dc9b6195040e785b8c3
Thomas Bach <thbach@students.uni-mainz.de>
2012-10-13 17:35:58 +0200

    new API

--- midas/tests/test_tools.py
+++ midas/tests/test_tools.py
@@ -4,9 +4,9 @@ from vincetools.compat import unittest
 
 class GroupByKeyTests(unittest.TestCase):
 
-    def _run_it(self, iterable):
+    def _run_it(self, iterable, keyfunc=lambda x: x.split('\t', 1)[0]):
         from midas.tools import group_by_key
-        return group_by_key(iterable)
+        return group_by_key(iterable, keyfunc)
 
     def test_general_case(self):
         data = ('{0}\t3'.format(e) for e in ['foo'] * 5 + ['bar'] * 3)
