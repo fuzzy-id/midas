012ec7ac161f824a3532fed343a4c8c3395edea6
Thomas Bach <thbach@students.uni-mainz.de>
2012-10-13 20:23:57 +0200

    test count by key

--- midas/tests/test_tools.py
+++ midas/tests/test_tools.py
@@ -2,11 +2,19 @@
 
 from vincetools.compat import unittest
 
-class GroupByKeyTests(unittest.TestCase):
+class KeyFuncTestCase(unittest.TestCase):
 
-    def _run_it(self, iterable, keyfunc=lambda x: x.split('\t', 1)[0]):
+    @property
+    def _keyfunc(self):
+        from midas.tools import get_key
+        return get_key
+
+
+class GroupByKeyTests(KeyFuncTestCase):
+
+    def _run_it(self, iterable, keyfunc=None):
         from midas.tools import group_by_key
-        return group_by_key(iterable, keyfunc)
+        return group_by_key(iterable, self._keyfunc)
 
     def test_general_case(self):
         data = ('{0}\t3'.format(e) for e in ['foo'] * 5 + ['bar'] * 3)
@@ -16,3 +24,20 @@ class GroupByKeyTests(unittest.TestCase):
     def test_single_key(self):
         result = [ list(block) for block in self._run_it(['foo\t3']*1) ]
         self.assertEqual(result, [['foo\t3']*1])
+
+class CountByKeyTests(KeyFuncTestCase):
+
+    def _run_it(self, iterable):
+        from midas.tools import count_by_key
+        return count_by_key(iterable, self._keyfunc)
+
+    def test_on_empty_sequence(self):
+        self.assertEqual(dict(), self._run_it([]))
+
+    def test_on_sequence(self):
+        sequence = [ '{0}\tblah'.format(key)
+                     for key in ['foo']*2 + ['bar']*1 ]
+        self.assertEqual(dict([('foo', 2), 
+                               ('bar', 1)]),
+                         self._run_it(sequence))
+
