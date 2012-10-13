dce6ebd8c9e1c695f5bfe2f5b81a9a6c0a00d3a4
Thomas Bach <thbach@students.uni-mainz.de>
2012-10-13 15:19:10 +0200

    basic setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='vincetools',
+      version='0.1dev',
+      packages=['vincetools', 'vincetools.compat'],
+      license='BSD-new')
