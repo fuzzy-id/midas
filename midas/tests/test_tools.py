4ace05accd2d501bf94f60c0379dfceae68c48ce
Thomas Bach <thbach@students.uni-mainz.de>
2012-10-13 20:34:09 +0200

    testing interactive functions

--- midas/tests/test_tools.py
+++ midas/tests/test_tools.py
@@ -9,21 +9,28 @@ class KeyFuncTestCase(unittest.TestCase):
         from midas.tools import get_key
         return get_key
 
+    sequence = [ '{0}\tblah'.format(e) 
+                 for e in ['foo'] * 5 + ['bar'] * 3 ]
+
 
 class GroupByKeyTests(KeyFuncTestCase):
 
-    def _run_it(self, iterable, keyfunc=None):
+    def _run_it(self, iterable):
         from midas.tools import group_by_key
-        return group_by_key(iterable, self._keyfunc)
+        return [ list(block) 
+                 for block in group_by_key(iterable, self._keyfunc) ]
 
-    def test_general_case(self):
-        data = ('{0}\t3'.format(e) for e in ['foo'] * 5 + ['bar'] * 3)
-        result = [ list(block) for block in self._run_it(data) ]
-        self.assertEqual(result, [['foo\t3']*5, ['bar\t3']*3])
+    def test_on_empty_sequence(self):
+        self.assertEqual([], self._run_it([]))
+
+    def test_on_sequence(self):
+        self.assertEqual([['foo\tblah']*5, ['bar\tblah']*3], 
+                         self._run_it(self.sequence))
         
     def test_single_key(self):
         result = [ list(block) for block in self._run_it(['foo\t3']*1) ]
-        self.assertEqual(result, [['foo\t3']*1])
+        self.assertEqual([['foo\t3']*1], result)
+
 
 class CountByKeyTests(KeyFuncTestCase):
 
@@ -35,9 +42,21 @@ class CountByKeyTests(KeyFuncTestCase):
         self.assertEqual(dict(), self._run_it([]))
 
     def test_on_sequence(self):
-        sequence = [ '{0}\tblah'.format(key)
-                     for key in ['foo']*2 + ['bar']*1 ]
-        self.assertEqual(dict([('foo', 2), 
-                               ('bar', 1)]),
-                         self._run_it(sequence))
+        self.assertEqual(dict([('foo', 5), 
+                               ('bar', 3)]),
+                         self._run_it(self.sequence))
 
+
+class CollectByKeyTests(KeyFuncTestCase):
+
+    def _run_it(self, iterable):
+        from midas.tools import collect_by_key
+        return collect_by_key(iterable, self._keyfunc)
+
+    def test_on_empty_sequence(self):
+        self.assertEqual(dict(), self._run_it([]))
+
+    def test_on_sequence(self):
+        self.assertEqual(dict([('foo', ['foo\tblah']*5),
+                               ('bar', ['bar\tblah']*3)]), 
+                         self._run_it(self.sequence))
