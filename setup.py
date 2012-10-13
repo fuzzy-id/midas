4ee5e2d86268e5a25c2cc4573ac6bada23ee0718
Thomas Bach <thbach@students.uni-mainz.de>
2012-10-13 15:51:11 +0200

    we provide unittest also

--- setup.py
+++ setup.py
@@ -2,8 +2,16 @@
 
 from setuptools import setup
 
+import sys
+
+install_requires = []
+
+if sys.version_info[:2] == (2, 6):
+    install_requires.append('unittest2')
+
 setup(name='vincetools',
       version='0.1dev',
       packages=['vincetools'],
       license='BSD-new',
-      test_suite='vincetools.tests')
+      test_suite='vincetools.tests',
+      install_requires=install_requires)
