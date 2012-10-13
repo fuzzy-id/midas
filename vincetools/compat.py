124943ac2df4eb32bc763fd93e70461e627597af
Thomas Bach <thbach@students.uni-mainz.de>
2012-10-13 14:45:59 +0200

    compat module added

---
+++ vincetools/compat.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+""" This module provides (mostly) wrappers to let our code run on
+Python 2 and Python 3.
+"""
+
+import sys
+
+#: True if the current interpreter is of version 2.6
+PY26 = sys.version_info[:2] == (2, 6)
+#: True if the current interpreter is of version 3
+PY3K = sys.version_info[0] == 3
+
+if PY26:  # pragma: no cover
+    import zipfile
+    class ZipFile(zipfile.ZipFile):
+        
+        def __enter__(self):
+            return self
+
+        def __exit__(self, *exc_inf):
+            self.close()
+            return True
+else:
+    from zipfile import ZipFile
+
+if PY26:  # pragma: no cover
+    import gzip
+    class GzipFile(gzip.GzipFile):
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, *exc_inf):
+            self.close()
+            return True
+else:
+    from gzip import GzipFile
+
+if PY3K:  # pragma: no cover
+    imap = map
+else:
+    from itertools import imap
+
+if PY3K:  # pragma: no cover
+    ifilter = filter
+else:
+    from itertools import ifilter
+
+if PY3K:  # pragma: no cover
+    from urllib.parse import urlparse
+else:
+    from urlparse import urlparse
+
+if PY3K:  # pragma: no cover
+    from io import StringIO
+else:
+    from StringIO import StringIO
+
+if PY3K:  # pragma: no cover
+    from configparser import ConfigParser
+else:
+    from ConfigParser import SafeConfigParser
+
+    class ConfigParser(SafeConfigParser):
+
+        def read_string(self, s):
+            """ Puts the string `s` into a :class:`StringIO.StringIO`
+            instance and passes it to :meth:`readfp`.
+            """
+            buf = StringIO(s)
+            buf.seek(0)
+            self.readfp(buf)
