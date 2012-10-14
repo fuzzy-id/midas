6686c5db3238aa1f087ae9b6032a78514472a9a7
Thomas Bach <thbach@students.uni-mainz.de>
2012-10-14 16:49:53 +0200

    increase coverage

--- midas/tests/test_associate.py
+++ midas/tests/test_associate.py
@@ -4,6 +4,7 @@ import operator
 
 from vincetools.compat import imap
 from vincetools.compat import unittest
+from midas.tests import ConfiguredDBTestCase
 
 class AssociationTreeTests(unittest.TestCase):
 
@@ -57,6 +58,31 @@ class SplitDomainTests(unittest.TestCase):
 
     def test_on_empty_str(self):
         self.assertEqual(self._run_it(''), ('', ))
+
+class AssociateSitesToCompaniesTests(ConfiguredDBTestCase):
+
+    def _run_it(self, iterable=None):
+        from midas.associate import associate_sites_to_companies
+        return associate_sites_to_companies(iterable)
+
+    def test_on_test_config(self):
+        foo_comp = self._make_company_json(self.companies_js[0])
+        bar_comp = self._make_company_json(self.companies_js[1])
+        self.assertEqual(self._run_it(), 
+                         {foo_comp: ['foo.example.com']})
+
+
+class AssociateCompaniesToSitesTests(ConfiguredDBTestCase):
+
+    def _run_it(self, iterable=None):
+        from midas.associate import associate_companies_to_sites
+        return associate_companies_to_sites(iterable)
+
+    def test_on_test_config(self):
+        foo_comp = self._make_company_json(self.companies_js[0])
+        bar_comp = self._make_company_json(self.companies_js[1])
+        self.assertEqual(self._run_it(), 
+                         {'foo.example.com': [foo_comp]})
 
 if __name__ == '__main__':  # pragma: no cover
     unittest.main()
