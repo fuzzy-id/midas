4855fcd52f013dfd3ab2c54ec529791455af8c1d
Thomas Bach <thbach@students.uni-mainz.de>
2012-10-14 17:14:53 +0200

    cleaning up

--- midas/tests/test_rank_entry.py
+++ midas/tests/test_rank_entry.py
@@ -3,6 +3,7 @@
 import datetime
 
 from vincetools.compat import unittest
+
 from midas.tests import TEST_DATA
 
 class RankEntryTestCase(unittest.TestCase):
@@ -16,24 +17,6 @@ class RankEntryTestCase(unittest.TestCase):
 
     def _make_one(self, site, date, rank):
         return self._get_target_cls()(site=site, date=date, rank=rank)
-
-
-class FormatTests(RankEntryTestCase):
-
-    def test_format_std(self):
-        entry = self._make_one('foo', datetime.datetime(1900, 1, 1), 1)
-        self.assertEqual(entry.format_std, 'foo\t1900-01-01, 1')
-
-    def test_format_w_key(self):
-        entry = self._make_one('foo', datetime.datetime(1900, 1, 1), 1)
-        self.assertEqual(entry.format_w_key, '0be\tfoo\t1900-01-01, 1')
-
-    def test_str_representation(self):
-        entry = self._make_one('foo', self.a_date, 1)
-        self.assertEqual(str(entry), 'RankEntry(foo, 1900-01-01, 1)')
-
-
-class ComparisonTests(RankEntryTestCase):
 
     def assert_lt_le_neq(self, lesser, greater):
         self.assertTrue(lesser <  greater)
@@ -49,37 +32,49 @@ class ComparisonTests(RankEntryTestCase):
         self.assertFalse(lesser >= greater)
         self.assertFalse(lesser >  greater)
 
-    def test_different_names_remaining_same(self):
+    def test_format_std(self):
+        entry = self._make_one('foo', self.a_date, 1)
+        self.assertEqual(entry.format_std, 'foo\t1900-01-01, 1')
+
+    def test_format_w_key(self):
+        entry = self._make_one('foo', self.a_date, 1)
+        self.assertEqual(entry.format_w_key, '0be\tfoo\t1900-01-01, 1')
+
+    def test_str_representation(self):
+        entry = self._make_one('foo', self.a_date, 1)
+        self.assertEqual(str(entry), 'RankEntry(foo, 1900-01-01, 1)')
+
+    def test_cmp_different_names_remaining_same(self):
         a = self._make_one('bar', self.a_date, 1)
         b = self._make_one('foo', self.a_date, 1)
         self.assert_lt_le_neq(a, b)
 
-    def test_different_names_and_dates(self):
+    def test_cmp_different_names_and_dates(self):
         a = self._make_one('bar', self.a_date + self.one_day, 1)
         b = self._make_one('foo', self.a_date, 1)
         self.assert_lt_le_neq(a, b)
 
-    def test_different_names_and_ranks(self):
+    def test_cmp_different_names_and_ranks(self):
         a = self._make_one('bar', self.a_date, 2)
         b = self._make_one('foo', self.a_date, 1)
         self.assert_lt_le_neq(a, b)
 
-    def test_different_names_dates_and_ranks(self):
+    def test_cmp_different_names_dates_and_ranks(self):
         a = self._make_one('bar', self.a_date + self.one_day, 2)
         b = self._make_one('foo', self.a_date, 1)
         self.assert_lt_le_neq(a, b)
 
-    def test_different_dates_remaining_same(self):
+    def test_cmp_different_dates_remaining_same(self):
         a = self._make_one('bar', self.a_date, 1)
         b = self._make_one('bar', self.a_date + self.one_day, 1)
         self.assert_lt_le_neq(a, b)
 
-    def test_different_dates_and_ranks(self):
+    def test_cmp_different_dates_and_ranks(self):
         a = self._make_one('bar', self.a_date, 2)
         b = self._make_one('bar', self.a_date + self.one_day, 1)
         self.assert_lt_le_neq(a, b)
 
-    def test_sort_a_list(self):
+    def test_cmp_sort_a_list(self):
         a = self._make_one('foo', self.a_date, 2)
         b = self._make_one('foo', self.a_date + self.one_day, 1)
         c = self._make_one('foo', self.a_date + self.one_day*2, 1)
@@ -88,8 +83,6 @@ class ComparisonTests(RankEntryTestCase):
         entries.sort()
         self.assertEqual(entries, [a, b, c, d])
 
-class IterAlexaFileTests(RankEntryTestCase):
-
-    def test_on_example_data(self):
+    def test_iter_example_alexa_file(self):
         result = tuple(self._get_target_cls().iter_alexa_file(TEST_DATA[0]))
         self.assertEqual(result, TEST_DATA[1])
