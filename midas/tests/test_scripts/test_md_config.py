5ac65bf52979f7ef506d9f4eb5dcb0060d5e2183
Thomas Bach <thbach@students.uni-mainz.de>
2012-10-27 16:27:02 +0200

    coverage

--- midas/tests/test_scripts/test_md_config.py
+++ midas/tests/test_scripts/test_md_config.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import tempfile
+
+import vincetools.compat as vt_comp
+
 from midas.tests.test_scripts import IntegrationTestCase
 
 class MDConfigTests(IntegrationTestCase):
@@ -11,3 +15,15 @@ class MDConfigTests(IntegrationTestCase):
     def test_without_arguments(self):
         self.assertEqual(self._run_it(), 0)
         self.assert_stdout_startswith('[DEFAULT]\n')
+
+    def test_with_job_cfg(self):
+        with tempfile.NamedTemporaryFile('w+') as tmp:
+            tmp.writelines('\n'.join(['[DEFAULT]',
+                                      'user_name = foo']))
+            tmp.seek(0)
+            self.assertEqual(0, self._run_it(tmp.name))
+        self.assert_stdout_startswith('[DEFAULT]\n')
+        self.assert_in_stdout('user_name = foo')
+
+if __name__ == '__main__':  # pragma: no cover
+    unittest.main()
