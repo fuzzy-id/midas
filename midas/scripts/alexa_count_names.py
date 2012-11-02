30e9dbc638ffd5a2b3996fe821146d9f4990f843
Thomas Bach <thbach@students.uni-mainz.de>
2012-11-02 15:10:16 +0100

    using mrjob

--- midas/scripts/alexa_count_names.py
+++ midas/scripts/alexa_count_names.py
@@ -2,6 +2,8 @@
 
 import logging
 
+import mrjob.job
+
 from midas.scripts import MDJob
 from midas import RankEntry
 from midas.tools import group_by_key
@@ -10,27 +12,17 @@ from midas.tools import split_key_value
 
 logger = logging.getLogger(__name__)
 
+class AlexaCountSitesJob(mrjob.job.MRJob):
 
-class AlexaToNamesAndOne(MDJob):
-    """ Parse Alexa Top1M files and print the names found in the
-    entries as key and a `1' as value.
-    """
+    def mapper(self, key, fname):
+        """ Parse Alexa Top1M files and print the names found in the
+        entries as key and a `1' as value.
+        """
+        logger.info("processing '{0}'".format(fname))
+        for entry in RankEntry.iter_alexa_file(fname):
+            yield entry.site, 1
 
-    def run(self):
-        for fname in self.args.stream:
-            logger.info("processing '{0}'".format(fname))
-            for entry in RankEntry.iter_alexa_file(fname):
-                print("{0}\t1".format(entry.site))
-
-
-class SumValues(MDJob):
-    """ Sums up the counts of the keys.
-    """
-
-    def run(self):
-        for group in group_by_key(self.args.stream, get_key):
-            counter = 0
-            for name_tab_count in group:
-                name, count = split_key_value(name_tab_count)
-                counter += int(count)
-            print('{0}\t{1}'.format(name, counter))
+    def reducer(self, site, occurences):
+        """ Sums up the counts of the sites.
+        """
+        yield site, sum(occurences)
