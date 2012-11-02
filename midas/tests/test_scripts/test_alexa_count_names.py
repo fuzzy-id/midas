f97309e0d77e4e4e1b804547fee461f20d7777ee
Thomas Bach <thbach@students.uni-mainz.de>
2012-11-02 16:23:07 +0100

    using mrjob

--- midas/tests/test_scripts/test_alexa_count_names.py
+++ midas/tests/test_scripts/test_alexa_count_names.py
@@ -1,29 +1,36 @@
 # -*- coding: utf-8 -*-
 
+import vincetools.compat as vt_comp
+
 from midas.tests import TEST_ALEXA_TOP1M
 from midas.tests import TEST_ALEXA_TOP1M_FILES
-from midas.tests.test_scripts import IntegrationTestCase
 
-class AlexaToNamesAndOneTests(IntegrationTestCase):
+class AlexaCountSitesJobTests(vt_comp.unittest.TestCase):
 
-    def _get_target_func(self):
-        from midas.scripts.alexa_count_names import AlexaToNamesAndOne
-        return AlexaToNamesAndOne.cmd
+    def _get_target_cls(self):
+        from midas.scripts.alexa_count_sites import AlexaCountSitesJob
+        return AlexaCountSitesJob
 
-    def test_on_file(self):
-        ret_code = self._run_it('-q', *TEST_ALEXA_TOP1M_FILES)
-        self.assertEqual(ret_code, 0)
-        self.assert_stdout_equal(''.join('{0}\t1\n'.format(e.site)
-                                         for e in TEST_ALEXA_TOP1M))
+    def test_mapper_on_test_data(self):
+        j = self._get_target_cls()()
+        result = list()
+        for alexa_f in TEST_ALEXA_TOP1M_FILES:
+            result.extend(j.mapper(None, alexa_f))
+        self.assertEqual(sorted(result), 
+                         sorted((e.site, 1) for e in TEST_ALEXA_TOP1M))
 
-class SumValues(IntegrationTestCase):
+    def test_reducer_simple(self):
+        j = self._get_target_cls()()
+        result = j.reducer('foo', range(4))
+        self.assertEqual(next(result), ('foo', 6))
 
-    def _get_target_func(self):
-        from midas.scripts.alexa_count_names import SumValues
-        return SumValues.cmd
-
-    def test_simple(self):
-        ret_code = self._run_it(*('{0}\t1\n'.format(name) 
-                                  for name in ['foo'] * 5 + ['bar'] * 3))
-        self.assertEqual(ret_code, 0)
-        self.assert_stdout_equal('foo\t5\nbar\t3\n')
+    def test_job(self):
+        stdin = vt_comp.StringIO('\n'.join(TEST_ALEXA_TOP1M_FILES) + '\n')
+        j = self._get_target_cls()(['--no-conf', '-r', 'inline', '-'])
+        j.sandbox(stdin=stdin)
+        with j.make_runner() as runner:
+            runner.run()
+            result = [ line for line in runner.stream_output() ]
+        self.assertEqual(sorted(result), [ '"baz.bar.example.com/path"\t2\n',
+                                           '"foo.example.com"\t1\n' ])
+        
