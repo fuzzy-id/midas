0ce6da4c323e31882d3e233d4fec52290bbbc94c
Thomas Bach <thbach@students.uni-mainz.de>
2012-11-05 19:59:43 +0100

    refine test

--- midas/tests/test_scripts/test_alexa_zip_to_gzip.py
+++ midas/tests/test_scripts/test_alexa_zip_to_gzip.py
@@ -8,6 +8,7 @@ import vincetools.compat as vt_comp
 
 from midas.tests.test_scripts import IntegrationTestCaseNG
 
+import midas
 import midas.tests as md_tests
 
 class AlexaZipToGzipTests(IntegrationTestCaseNG):
@@ -26,12 +27,12 @@ class AlexaZipToGzipTests(IntegrationTestCaseNG):
 
     def test_on_test_data(self):
         ret_code = self._call_cmd(
-            os.path.basename(md_tests.TEST_ALEXA_TOP1M_FILES[0]),
+            os.path.dirname(md_tests.TEST_ALEXA_TOP1M_FILES[0]),
             self.tmpd
             )
         self.assertEqual(ret_code, 0)
-        first = os.path.join(self.tmpd, 'top-1m-2012-09-03.gz')
-        snd = os.path.join(self.tmpd, 'top-1m-2012-09-04.gz')
+        first = os.path.join(self.tmpd, 'top_1m_2012-09-03.gz')
+        snd = os.path.join(self.tmpd, 'top_1m_2012-09-04.gz')
         self.assertTrue(os.path.isfile(first))
         self.assertTrue(os.path.isfile(snd))
         with vt_comp.GzipFile(first) as fp:
@@ -41,4 +42,4 @@ class AlexaZipToGzipTests(IntegrationTestCaseNG):
         with vt_comp.GzipFile(snd) as fp:
             entries = [ midas.RankEntry.parse_json(l.decode('utf-8'))
                         for l in fp ]
-        self.assertEqual(line, md_tests.TEST_ALEXA_TOP1M[2:])
+        self.assertEqual(entries, md_tests.TEST_ALEXA_TOP1M[2:])
