c7152bd39e4de71ea806ced21c91166ff6b5aa5d
Thomas Bach <thbach@students.uni-mainz.de>
2012-11-05 11:52:11 +0100

    testing for existence of help flag

--- midas/tests/test_scripts/test_alexa_zip_to_gzip.py
+++ midas/tests/test_scripts/test_alexa_zip_to_gzip.py
@@ -1,1 +1,15 @@
 # -*- coding: utf-8 -*-
+
+from midas.tests.test_scripts import IntegrationTestCase
+
+class AlexaZipToGzipTests(IntegrationTestCase):
+
+    def _get_target_func(self):
+        from midas.scripts.alexa_zip_to_gzip import AlexaZipToGzip
+        return AlexaZipToGzip.cmd
+
+    def test_help_flag(self):
+        with self.assertRaises(SystemExit) as cm:
+            self._run_it('-h')
+        self.assertEqual(cm.exception.code, 0)
+        self.assert_stdout_startswith('usage: ')
