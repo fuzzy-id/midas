03eae5db994a2e43ead312a3d08ccd3ed1592df7
Thomas Bach <thbach@students.uni-mainz.de>
2012-11-05 18:48:36 +0100

    we use json for ease of use with Pig

--- midas/tests/test_scripts/test_alexa_zip_to_gzip.py
+++ midas/tests/test_scripts/test_alexa_zip_to_gzip.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import shutil
+import os.path
 import tempfile
 
 import vincetools.compat as vt_comp
 
 from midas.tests.test_scripts import IntegrationTestCaseNG
+
+import midas.tests as md_tests
 
 class AlexaZipToGzipTests(IntegrationTestCaseNG):
 
@@ -21,17 +24,22 @@ class AlexaZipToGzipTests(IntegrationTestCaseNG):
         from midas.scripts.alexa_zip_to_gzip import AlexaZipToGzip
         return AlexaZipToGzip
 
+    @vt_comp.unittest.skip('Not ready, yet!')
     def test_on_test_data(self):
-        ret_code = self._call_cmd(*md_tests.TEST_ALEXA_TOP1M_FILES, self.tmpd)
+        ret_code = self._call_cmd(
+            os.path.basename(md_tests.TEST_ALEXA_TOP1M_FILES[0]),
+            self.tmpd
+            )
         self.assertEqual(ret_code, 0)
         first = os.path.join(self.tmpd, 'top-1m-2012-09-03.gz')
         snd = os.path.join(self.tmpd, 'top-1m-2012-09-04.gz')
         self.assertTrue(os.path.isfile(first))
         self.assertTrue(os.path.isfile(snd))
         with vt_comp.GzipFile(first) as fp:
-            lines = [ l.decode('utf-8') for l in fp ]
-        self.assertEqual(line, [ 'foo.example.com,1',
-                                 'baz.bar.example.com/path,2' ])
+            entries = [ midas.RankEntry.parse_json(l.decode('utf-8'))
+                        for l in fp ]
+        self.assertEqual(entries, md_tests.TEST_ALEXA_TOP1M[:2])
         with vt_comp.GzipFile(snd) as fp:
-            lines = [ l.decode('utf-8') for l in fp ]
-        self.assertEqual(line, [ 'baz.bar.example.com/path,1' ])
+            entries = [ midas.RankEntry.parse_json(l.decode('utf-8'))
+                        for l in fp ]
+        self.assertEqual(line, md_tests.TEST_ALEXA_TOP1M[2:])
