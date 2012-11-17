5b4ab2e9b7e0ec1ab7f6ab0c0aa79dff9b2020a7
Thomas Bach <thbach@students.uni-mainz.de>
2012-11-17 12:24:27 +0100

    finished implementation

--- midas/scripts/invert_index.py
+++ midas/scripts/invert_index.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import mrjob.job as mr_job
+import mrjob.protocol as mr_prot
 
 class InvertIndex(mr_job.MRJob):
 
-    def mapper(self, key, value):
-        return value['site'], (value['tstamp'], value['rank'])
+    INPUT_PROTOCOL = mr_prot.JSONValueProtocol
+
+    def mapper(self, _, rank_json):
+        yield rank_json['site'], (rank_json['tstamp'], rank_json['rank'])
+
+    def reducer(self, site, tstamp_ranks):
+        yield (site, sorted(tstamp_ranks))
--- midas/tests/test_scripts/test_invert_index.py
+++ midas/tests/test_scripts/test_invert_index.py
@@ -10,12 +10,37 @@ class InvertIndexTests(vt_comp.unittest.TestCase):
 
     def test_mapper(self):
         j = self._get_target_cls()()
-        key, value = j.mapper(None, {'site': 'foo.example.com', 
-                                     'rank': 1,
-                                     'tstamp': '2012-11-17'})
+        key, value = next(j.mapper(None, {'site': 'foo.example.com', 
+                                          'rank': 1,
+                                          'tstamp': '2012-11-17'}))
         self.assertEqual(key, 'foo.example.com')
         self.assertEqual(value, ('2012-11-17', 1))
 
+    def test_reducer(self):
+        j = self._get_target_cls()()
+        key, value = next(j.reducer('foo.example.com', [('2012-11-18', 3),
+                                                        ('2012-11-17', 2)]))
+        self.assertEqual(key, 'foo.example.com')
+        self.assertEqual(value, [('2012-11-17', 2), ('2012-11-18', 3)])
+
     def test_job(self):
+        stdin = vt_comp.StringIO(
+            '\n'.join([
+                    '{"site": "foo.example.com", "rank": 2, "tstamp": "2012-11-17"}',
+                    '{"site": "baz.bar.example.com/path", "rank": 1, "tstamp": "2012-11-17"}',
+                    '{"site": "foo.example.com", "rank": 3, "tstamp": "2012-11-18"}',
+                    '{"site": "baz.bar.example.com/path", "rank": 2, "tstamp": "2012-11-18"}',
+                    ]) + '\n')
         j = self._get_target_cls()(['-r', 'inline', '--no-conf', '-'])
-        
+        j.sandbox(stdin=stdin)
+        with j.make_runner() as runner:
+            runner.run()
+            result = [ j.parse_output_line(line) 
+                       for line in runner.stream_output() ]
+        self.assertEqual(
+            sorted(result),
+            sorted(
+                [("foo.example.com", [["2012-11-17", 2], ["2012-11-18", 3]]),
+                 ("baz.bar.example.com/path", [["2012-11-17", 1], ["2012-11-18", 2]])]
+                )
+            )
