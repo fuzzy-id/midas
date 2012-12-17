9dcc9ad8c483fa61191b6c076126c552bff536c2
Thomas Bach <thbach@students.uni-mainz.de>
2012-12-17 12:23:53 -0800

    adding current dir to working dir

--- pig_scripts/python_wrapper.sh
+++ pig_scripts/python_wrapper.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 . /home/thomasba/py_envs/py26_sys/bin/activate
-MPLCONFIGDIR=/home/thomasba/.matplotlib
+MPLCONFIGDIR="/home/thomasba/.matplotlib"
 export MPLCONFIGDIR
+
+if [ -z $CLASSPATH ]; then
+    CLASSPATH="."
+else
+    CLASSPATH=".:${CLASSPATH}"
+fi
+export CLASSPATH
+
 python $@
