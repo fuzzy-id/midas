960a020a0214286317304887361f731a8af816a6
Thomas Bach <thbach@students.uni-mainz.de>
2012-12-21 19:13:34 +0100

    check against test data file

--- pig_scripts/TestCountEntriesPerSite.java
+++ pig_scripts/TestCountEntriesPerSite.java
@@ -1,4 +1,5 @@
 import org.apache.pig.tools.parameters.ParseException;
+import java.io.File;
 import java.io.IOException;
 import org.apache.pig.pigunit.PigTest;
 import org.junit.Test;
@@ -7,20 +8,29 @@ public class TestCountEntriesPerSite {
     private PigTest test;
     private static final String SCRIPT = "count_entries_per_site.pig";
 
-    @Test public void testSampleData() throws IOException, ParseException {
-	PigTest test = new PigTest(SCRIPT);
-
+    @Test public void testSomeProvidedData() throws IOException, ParseException {
+	String[] args = {
+	    "input=provided_by_test",
+	    "output=site_count",
+	};
+	PigTest test = new PigTest(SCRIPT, args);
 	String[] input = {
 	    "foo.example.com\t{(2012-12-16,1),(2012-12-15,1),(2012-12-14,1)}",
 	    "bar.example.com\t{(2012-12-16,2),(2012-12-15,2),(2012-12-14,2)}",
 	};
-
 	String[] output = {
 	    "(foo.example.com,3)",
 	    "(bar.example.com,3)",
 	};
-
 	test.assertOutput("sites", input, "site_count", output);
     }
 	
+    @Test public void testOnTestData() throws IOException, ParseException {
+	String[] args = {
+	    "input=../test_data/alexa_grouped_by_site/data",
+	    "output=site_count",
+	};
+	PigTest test = new PigTest(SCRIPT, args);
+	test.assertOutput(new File("../test_data/site_count/expected_by_pig"));
+    }
 }
