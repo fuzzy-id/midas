9863d456257f7888e072a19e434f0ed8dc178cdd
Thomas Bach <thbach@students.uni-mainz.de>
2012-12-21 22:56:53 +0100

    gardening

--- midas/tests/__init__.py
+++ midas/tests/__init__.py
@@ -1,12 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import datetime
 import os.path
-import sys
-
-from midas.tools import SiteCount
-from midas.compat import StringIO
-from midas.compat import unittest
 
 _here = os.path.abspath(os.path.dirname(__file__))
 _test_data_home = os.path.join(_here, '..', '..', 'test_data')
