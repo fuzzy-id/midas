0a94ed96f3d3f1638aa4cd5ee6332acbd5f28e37
Thomas Bach <thbach@students.uni-mainz.de>
2012-12-24 10:30:44 +0100

    updating a bit

--- midas/scripts/md_associate.py
+++ midas/scripts/md_associate.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import midas.compat as vt_comp
+from midas.compat import d_iteritems
 
 import midas.associate as md_assoc
-import midas.db as md_db
 import midas.scripts as md_scripts
 
 class MDAssociate(md_scripts.MDCommand):
@@ -12,10 +11,6 @@ class MDAssociate(md_scripts.MDCommand):
 
     def run(self):
         s2c = md_assoc.associate_companies_to_sites()
-        sess = md_db.db_session()
-        for comp, sites in vt_comp.d_iteritems(s2c):
+        for comp, sites in d_iteritems(s2c):
             for site in sites:
-                #association = md_db.Association(company=comp, site=site)
-                #sess.add(association)
                 print(','.join([comp.permalink, site]))
-        #sess.commit()
