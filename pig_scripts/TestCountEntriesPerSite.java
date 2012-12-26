cf16d8a6978abcc0895f4e0fce1104630ad64094
Thomas Bach <thbach@students.uni-mainz.de>
2012-12-26 14:51:10 +0100

    gardening

--- pig_scripts/TestCountEntriesPerSite.java
+++ pig_scripts/TestCountEntriesPerSite.java
@@ -1,7 +1,7 @@
-import org.apache.pig.tools.parameters.ParseException;
 import java.io.File;
 import java.io.IOException;
 import org.apache.pig.pigunit.PigTest;
+import org.apache.pig.tools.parameters.ParseException;
 import org.junit.Test;
 
 public class TestCountEntriesPerSite {
