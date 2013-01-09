5ec0034cd5524bfc00c778fe8d274d034cefad6a
Thomas Bach <thbach@students.uni-mainz.de>
2013-01-09 18:53:56 +0100

    filtering adult sites

--- pig_scripts/TestGroupAlexaBySite.java
+++ pig_scripts/TestGroupAlexaBySite.java
@@ -8,35 +8,14 @@ public class TestGroupAlexaBySite {
     private PigTest test;
     private static final String SCRIPT = "group_alexa_by_site.pig";
 
-    @Test public void testSampleData() throws IOException, ParseException {
-	String[] args = {
-	    "input=provided_by_test",
-	    "output=rows",
-	};
-	PigTest test = new PigTest(SCRIPT, args);
-	String[] input = {
-	    "foo.example.com\t2012-12-16\t1",
-	    "foo.example.com\t2012-12-15\t1",
-	    "foo.example.com\t2012-12-14\t1",
-	    "bar.example.com\t2012-12-16\t2",
-	    "bar.example.com\t2012-12-15\t2",
-	    "bar.example.com\t2012-12-14\t2",
-	};
-	String[] output = {
-	    "(bar.example.com,{(2012-12-16,2),(2012-12-15,2),(2012-12-14,2)})",
-	    "(foo.example.com,{(2012-12-16,1),(2012-12-15,1),(2012-12-14,1)})",
-	};
-	test.assertOutput("top1m", input, "rows", output);
-    }
-
     @Test public void testTestDataWithFixedOutput() throws IOException, ParseException {
 	String[] args = {
 	    "input=../test_data/alexa_files/",
+	    "adult_sites=../test_data/adult_sites/",
 	    "output=rows",
 	};
 	String[] expected = {
 	    "(foo.example.com,{(2012-09-03,1)})",
-	    "(baz.bar.example.com/path,{(2012-09-03,2),(2012-09-04,1)})",
 	};
 	PigTest test = new PigTest(SCRIPT, args);
 	test.assertOutput("rows", expected);
@@ -46,6 +25,7 @@ public class TestGroupAlexaBySite {
 	String[] args = {
 	    "reducers=1",
 	    "input=../test_data/alexa_files/",
+	    "adult_sites=../test_data/adult_sites/",
 	    "output=rows",
 	};
 	PigTest test = new PigTest(SCRIPT, args);
--- pig_scripts/group_alexa_by_site.pig
+++ pig_scripts/group_alexa_by_site.pig
@@ -1,7 +1,13 @@
 top1m = LOAD '$input' AS
       (site:chararray, tstamp:chararray, rank:int);
 
-sites = GROUP top1m BY site;
-rows = FOREACH sites GENERATE group, top1m.(tstamp, rank);
+adult_sites = LOAD '$adult_sites' AS site: chararray;
+
+wo_path = FILTER top1m BY (NOT site MATCHES '.+/.+');
+joined = JOIN wo_path BY site LEFT OUTER, adult_sites BY site USING 'replicated';
+not_adult_sites = FILTER joined BY adult_sites::site is null;
+
+sites = GROUP not_adult_sites BY wo_path::site;
+rows = FOREACH sites GENERATE group, not_adult_sites.(wo_path::tstamp, wo_path::rank);
 
 STORE rows INTO '$output';
