3c384a7ed697b0124532d8f027e3db3af2ed8cc0
Thomas Bach <thbach@students.uni-mainz.de>
2013-01-11 17:46:11 +0100

    testing on test data

--- midas/tests/test_scripts/test_make_sample_restriction_shelve.py
+++ midas/tests/test_scripts/test_make_sample_restriction_shelve.py
@@ -1,2 +1,31 @@
 # -*- coding: utf-8 -*-
 
+import os.path
+import shelve
+
+import pandas
+
+from midas.tests import TEST_DATA_PATH
+from midas.tests.test_scripts import IntegrationTestCase
+
+class Tests(IntegrationTestCase):
+
+    def _get_target_cls(self):
+        from midas.scripts.sample_restriction import MakeSampleRestrictionShelve
+        return MakeSampleRestrictionShelve
+
+    def test_on_test_data(self):
+        shelf = os.path.join(self.tmpd, 'shelf')
+        self.assertEqual(
+            self._call_cmd('--shelf', shelf, TEST_DATA_PATH['sites_w_company']),
+            0
+            )
+        d = shelve.open(shelf)
+        self.assertEqual(len(d), 1)
+        self.assertTrue(d.has_key('foo'))
+        restr = d['foo']
+        self.assertEqual(restr.tstamp, pandas.Timestamp('2012-09-03'))
+        self.assertEqual(restr.date_lower, pandas.Timestamp('2012-08-31'))
+        self.assertEqual(restr.date_upper, pandas.Timestamp('2012-09-06'))
+        self.assertEqual(restr.rank_lower, 0)
+        self.assertEqual(restr.rank_upper, 2)
