6845f06d5ab55d1228ae9cad23d59ad178f5961b
Thomas Bach <thbach@students.uni-mainz.de>
2013-01-11 16:23:39 +0100

    needing restrictions

--- midas/tests/test_restrictions.py
+++ midas/tests/test_restrictions.py
@@ -2,11 +2,10 @@
 
 import datetime
 
-#import pandas
+import pandas
 
 from midas.compat import unittest
 
-@unittest.skip('numpy fails to compile with current distribute')
 class MeanRankInRangeAtDateTests(unittest.TestCase):
 
     def _make_one(self, *args, **kwargs):
@@ -15,19 +14,25 @@ class MeanRankInRangeAtDateTests(unittest.TestCase):
 
     def test_fulfill(self):
         restr = self._make_one(datetime.date(2012, 12, 24), 10, 12)
-        s = pandas.Series([8, 11, 15], pandas.date_range('2012-12-22', '2012-12-24'))
+        s = pandas.Series([8, 11, 15], 
+                          pandas.date_range('2012-12-22', '2012-12-24'))
         self.assertTrue(restr.fulfills(s))
-        s = pandas.Series([13, 11, 15], pandas.date_range('2012-12-22', '2012-12-24'))
+        s = pandas.Series([13, 11, 15], 
+                          pandas.date_range('2012-12-22', '2012-12-24'))
         self.assertFalse(restr.fulfills(s))
-        s = pandas.Series([8, 11, 15], pandas.date_range('2010-12-22', '2010-12-24'))
+        s = pandas.Series([8, 11, 15],
+                          pandas.date_range('2010-12-22', '2010-12-24'))
         self.assertFalse(restr.fulfills(s))
 
     def test_zero_days_offset(self):
         restr = self._make_one(datetime.date(2012, 12, 24), 10, 12, 
                                pandas.DateOffset(0))
-        s = pandas.Series([8, 11, 12], pandas.date_range('2012-12-22', '2012-12-24'))
+        s = pandas.Series([8, 11, 12], 
+                          pandas.date_range('2012-12-22', '2012-12-24'))
         self.assertTrue(restr.fulfills(s))
-        s = pandas.Series([13, 11, 15], pandas.date_range('2012-12-22', '2012-12-24'))
+        s = pandas.Series([13, 11, 15], 
+                          pandas.date_range('2012-12-22', '2012-12-24'))
         self.assertFalse(restr.fulfills(s))
-        s = pandas.Series([8, 11, 15], pandas.date_range('2010-12-22', '2010-12-24'))
+        s = pandas.Series([8, 11, 15], 
+                          pandas.date_range('2010-12-22', '2010-12-24'))
         self.assertFalse(restr.fulfills(s))
