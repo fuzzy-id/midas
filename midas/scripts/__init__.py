2c0755e6db64fe81987035d2f3962a7c3bf92c16
Thomas Bach <thbach@students.uni-mainz.de>
2013-01-13 13:18:33 +0100

    unifying style

--- midas/scripts/__init__.py
+++ midas/scripts/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-""" This module provides classes to easily create scripts which can be
+"""
+This module provides classes to easily create scripts which can be
 used from the command-line.
 """
 
@@ -37,8 +38,9 @@ class StoreSingleFileOrDirectoryAction(argparse.Action):
 
 
 class MDCommand(object):
-    """ Initializes the :class:`MDCommand` instance by passing `argv`
-    to :meth:`parser` and configuring :mod:`logging`. If `argv` is
+    """ 
+    Initializes the :class:`MDCommand` instance by passing `argv` to
+    :meth:`parser` and configuring :mod:`logging`. If `argv` is
     ``None`` :obj:`sys.argv` is passed to the parser.
 
     Subclass this to write an easy to use command line interface to
@@ -56,9 +58,10 @@ class MDCommand(object):
     
     @classmethod
     def cmd(cls, argv=None):
-        """ Initiates the class and calls its :meth:`run`
-        method. Returns whatever :meth:`run` returns, except for
-        :obj:`None` which is interpreted as `0` (integer zero).
+        """
+        Initiates the class and calls its :meth:`run` method. Returns
+        whatever :meth:`run` returns, except for :obj:`None` which is
+        interpreted as `0` (integer zero).
         """
         obj = cls(argv)
         ret_val = obj.run()
@@ -66,15 +69,17 @@ class MDCommand(object):
         return 0 if ret_val is None else ret_val
 
     def run(self):  # pragma: no cover
-        """ Overwrite this method with the code you want your command
-        to execute. Return an integer unequal `0` if you want to
-        signal that something went wrong.
+        """
+        Overwrite this method with the code you want your command to
+        execute. Return an integer unequal `0` if you want to signal
+        that something went wrong.
         """
         raise NotImplementedError()
 
     @property
     def parser(self):
-        """ Returns a :class:`argparse.ArgumentParser` object having
+        """
+        Returns a :class:`argparse.ArgumentParser` object having
         `--verbose`, `--quiet` flags and reads from `stdin`.
         """
         if not hasattr(self, '_parser'):
@@ -89,7 +94,8 @@ class MDCommand(object):
         return self._parser
 
     def add_argument(self):  # pragma: no cover
-        """ Overwrite this function to add further arguments to
+        """
+        Overwrite this function to add further arguments to
         :attr:`self.parser`. This function is called before the actual
         arguments are passed in :meth:`__init__`.
         """
