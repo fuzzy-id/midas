834064050f1a623042bf923946dac41c6797ed24
Thomas Bach <thbach@students.uni-mainz.de>
2013-01-20 17:17:05 +0100

    producing output in pig format

--- midas/scripts/verify_indicator_stream.py
+++ midas/scripts/verify_indicator_stream.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
 import argparse
 import sys
 import struct
@@ -30,6 +33,16 @@ def iter_features(fp, num_features):
             features.append((tstamp, bits[:num_features]))
         yield (site_id, features)
 
+def to_string(site, features):
+    return '{0}\t{{{1}}}'.format(
+        site, ','.join( 
+            '({0},({1}))'.format(
+                tstamp, ','.join(map(str, vectors.tolist())) 
+                )
+                for tstamp, vectors in features 
+            )
+        )
+
 def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument('num_features', type=int,
@@ -39,7 +52,7 @@ def main():
     args = argparser.parse_args(sys.argv[1:])
     with open(args.istream, 'rb') as fp:
         for site_id, features in iter_features(fp, args.num_features):
-            pprint.pprint((site_id, features))
+            print(to_string(site_id, features))
 
 if __name__ == "__main__":
     main()
