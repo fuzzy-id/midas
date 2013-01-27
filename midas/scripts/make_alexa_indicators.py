208d06100bf453e7fed7178a250d52270d3907ba
Thomas Bach <thbach@students.uni-mainz.de>
2013-01-27 14:13:12 +0100

    completed script

--- midas/scripts/make_alexa_indicators.py
+++ midas/scripts/make_alexa_indicators.py
@@ -1,20 +1,29 @@
 # -*- coding: utf-8 -*-
 
+import argparse
 import itertools
+import subprocess
+
+import yaml
 
 from midas.compat import d_iteritems
 from midas.scripts import MDCommand
+from midas.scripts.verify_indicator_stream import iter_features
+from midas.scripts.verify_indicator_stream import to_string
 
 FILTERS = set(['rsi', 'ols-slope', 'spearman', 'pearson' 'rank'])
 
 def expand_config(conf):
-    for filter_, args in d_iteritems(conf):
-        if not filter_ in FILTERS:
-            raise ValueError('Unknown filter: {0}'.format(filter_))
+    for filter_ in FILTERS:
+        if not filter_ in conf:
+            continue
+        args = conf[filter_]
         ndays = args['ndays']
         thresholds = args['thresholds']
         if isinstance(ndays, dict):
-            ndays = xrange(ndays['start'], ndays['stop'], ndays['step'])
+            ndays = xrange(ndays['start'], 
+                           ndays['stop'], 
+                           ndays.get('step', 1))
         if isinstance(thresholds, dict):
             thresholds = xrange(thresholds['start'], 
                                 thresholds['stop'], 
@@ -23,5 +32,46 @@ def expand_config(conf):
             yield (filter_, days, threshold)
             
 
+def generate_names(conf, classes=['seed', 'angel', 'a', 'negative']):
+    tpl = """
+class.
+
+site:\tlabel.
+class:\t{0}.
+{1}
+"""
+    return tpl.format(', '.join(classes),
+                      '\n'.join('{0}_{1}_{2}:\tTrue, False.'.format(*t)
+                                for t in expand_config(conf)))
+
+def call_stream_alexa_indicators(conf):
+    cmd = [conf['exec'], ]
+    num_features = 0
+    for args in expand_config(conf):
+        cmd.append(','.join(map(str, args)))
+        num_features += 1
+    subp = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    for features in iter_features(subp.stdout, num_features):
+        yield features
+    if subp.poll() != 0:  # pragma: no cover
+        raise Exception('Subprocess did not succeed: {0}'.format(subp.returncode))
+
 class MakeAlexaIndicators(MDCommand):
-    pass
+    """
+    Run `stream-alexa-indicators`.
+    """
+
+    def add_argument(self):
+        self.parser.add_argument('names_file', type=argparse.FileType('w'),
+                                 help="The `.names' file that shall be written")
+        self.parser.add_argument('indicators_file', type=argparse.FileType('w'),
+                                 help="The indicators file that shall be written")
+        self.parser.add_argument('yaml_file', type=argparse.FileType(),
+                                 help='Parameters-file to run `stream-alexa-indicators`')
+
+    def run(self):
+        conf = yaml.safe_load(self.args.yaml_file)
+        for site_id, features in call_stream_alexa_indicators(conf):
+            self.args.indicators_file.write(to_string(site_id, features))
+            self.args.indicators_file.write('\n')
+        self.args.names_file.write(generate_names(conf))
--- midas/tests/test_scripts/test_make_alexa_indicators.py
+++ midas/tests/test_scripts/test_make_alexa_indicators.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import os.path
+
+import bitarray
+import mock
+import yaml
+
+from midas.compat import StringIO
 from midas.compat import unittest
-from midas.tests.test_scripts import MDCommandTestCase
+from midas.tests.test_scripts import IntegrationTestCase
+
+CONF = {'exec': 'non_existent',
+        'rsi': {'ndays': {'start': 0,
+                          'stop': 3,
+                          'step': 2},
+                'thresholds': {'start': 0,
+                               'stop': 2}}}
 
 class ExpandConfigTests(unittest.TestCase):
 
@@ -11,11 +25,7 @@ class ExpandConfigTests(unittest.TestCase):
 
     def test_start_stop_step_on_ndays_and_threshold(self):
         func = self._get_target()
-        result = func({'rsi': {'ndays': {'start': 0,
-                                         'stop': 3,
-                                         'step': 2},
-                               'thresholds': {'start': 0,
-                                              'stop': 2}}})
+        result = func(CONF)
         expected = [('rsi', 0, 0), ('rsi', 0, 1), ('rsi', 2, 0), ('rsi', 2, 1)]
         self.assertEqual(list(result), expected)
 
@@ -25,3 +35,67 @@ class ExpandConfigTests(unittest.TestCase):
                                'thresholds': [0, 2]}})
         expected = [('rsi', 0, 0), ('rsi', 0, 2), ('rsi', 1, 0), ('rsi', 1, 2)]
         self.assertEqual(list(result), expected)
+
+
+class GenerateNamesTests(unittest.TestCase):
+
+    def _get_target(self):
+        from midas.scripts.make_alexa_indicators import generate_names
+        return generate_names
+
+    def test_some(self):
+        func = self._get_target()
+        result = func(CONF)
+        expected = """
+class.
+
+site:\tlabel.
+class:\tseed, angel, a, negative.
+rsi_0_0:\tTrue, False.
+rsi_0_1:\tTrue, False.
+rsi_2_0:\tTrue, False.
+rsi_2_1:\tTrue, False.
+"""
+        self.assertEqual(result, expected)
+
+
+class CallStreamAlexaIndicatorsTests(unittest.TestCase):
+
+    def _get_target(self):
+        from midas.scripts.make_alexa_indicators import call_stream_alexa_indicators
+        return call_stream_alexa_indicators
+
+    @mock.patch("subprocess.Popen")
+    def test_mocked_popen(self, Popen):
+        Popen().stdout = StringIO(b'\x01\x00\x00\x00\xb1\xb9\x02M\x07\x00\x00\x00\x00')
+        Popen().poll.return_value = 0
+        func = self._get_target()
+        result = list(func(CONF))
+        expected = [(1, [(1292024241, bitarray.bitarray('1110'))])]
+        self.assertEqual(result, expected)
+
+
+class IntegrationTests(IntegrationTestCase):
+
+    def _get_target_cls(self):
+        from midas.scripts.make_alexa_indicators import MakeAlexaIndicators
+        return MakeAlexaIndicators
+
+    @mock.patch("subprocess.Popen")
+    def test_mocked_popen(self, Popen):
+        Popen().stdout = StringIO(b'\x01\x00\x00\x00\xb1\xb9\x02M\x07\x00\x00\x00\x00')
+        Popen().poll.return_value = 0
+        names_f = os.path.join(self.tmpd, 'indicators.names')
+        indicators_f = os.path.join(self.tmpd, 'indicators')
+        conf_f = os.path.join(self.tmpd, 'conf.yml')
+        with open(conf_f, 'w') as fp:
+            fp.write(yaml.safe_dump(CONF))
+        self.assertEqual(self._call_cmd(names_f, indicators_f, conf_f), 0)
+        with open(indicators_f) as fp:
+            self.assertEqual(fp.read(), '1\t{(1292024241,(True,True,True,False))}\n')
+        with open(names_f) as fp:
+            self.assertTrue(fp.read().startswith('\nclass'))
+
+
+if __name__ == '__main__':  # pragma: no cover
+    unittest.main()
