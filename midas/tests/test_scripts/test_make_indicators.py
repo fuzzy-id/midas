c66ca2534d95d3d04dcd23006f4bef6d07ffcfd4
Thomas Bach <thbach@students.uni-mainz.de>
2013-01-29 21:35:50 +0100

    less namespace pollution

--- midas/tests/test_scripts/test_make_indicators.py
+++ midas/tests/test_scripts/test_make_indicators.py
@@ -27,7 +27,9 @@ class TestIterFeatures(unittest.TestCase):
         return iter_features
 
     def test_simple_example(self):
-        buf = BytesIO(b'\x01\x00\x00\x00\xb1\xb9\x02M\x07\x00\x00\x00\x00')
+        buf = io.BytesIO(
+            b'\x01\x00\x00\x00\xb1\xb9\x02M\x07\x00\x00\x00\x00'
+            )
         func = self.get_target()
         result = list(func(buf, 3))
         expected = [(1, [(1292024241, bitarray.bitarray('111'))])]
